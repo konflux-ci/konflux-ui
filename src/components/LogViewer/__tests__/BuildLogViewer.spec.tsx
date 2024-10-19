25cc7e192b2d5ec943da50d7df056388e6aa0bef
Sahil Budhwar <sahilbudhwar143@gmail.com>
2024-10-19 21:59:38 +0530

    fix: move and fix unit tests for LogViewer, Overview, Releases

---
+++ src/components/LogViewer/__tests__/BuildLogViewer.spec.tsx
@@ -0,0 +1,89 @@
+import { configure, screen } from '@testing-library/react';
+import { PipelineRunLabel, PipelineRunType } from '../../../consts/pipelinerun';
+import { useTRPipelineRuns } from '../../../hooks/useTektonResults';
+import { createK8sWatchResourceMock, routerRenderer } from '../../../utils/test-utils';
+import { componentCRMocks } from '../../ApplicationDetails/__data__/mock-data';
+import { pipelineRunMock } from '../__data__/pipelineRunMocks';
+import { BuildLogViewer } from '../BuildLogViewer';
+
+jest.mock('react-router-dom', () => {
+  const actual = jest.requireActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: jest.fn(),
+  };
+});
+jest.mock('../../../hooks/useTektonResults');
+
+const watchResourceMock = createK8sWatchResourceMock();
+const useTRPipelineRunsMock = useTRPipelineRuns as jest.Mock;
+
+describe('BuildLogViewer', () => {
+  beforeEach(() => {
+    configure({ testIdAttribute: 'data-test' });
+  });
+
+  it('should show empty box if pipelineRuns not found', () => {
+    watchResourceMock.mockReturnValue([[], true]);
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+    expect(screen.getByTestId('empty-message')).not.toBeNull();
+    expect(screen.getByTestId('empty-message').innerHTML).toBe('No pipeline runs found');
+  });
+
+  it('should show component name', () => {
+    watchResourceMock.mockReturnValue([[pipelineRunMock], true]);
+    watchResourceMock.mockReturnValue([[pipelineRunMock], true]);
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+    screen.getByText('basic-node-js');
+  });
+
+  it('should show pipeline run link', () => {
+    watchResourceMock.mockReturnValue([[pipelineRunMock], true]);
+    watchResourceMock.mockReturnValue([[pipelineRunMock], true]);
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+    const plrLink = screen.getByText('basic-node-js-7c8nd');
+    expect(plrLink.getAttribute('href')).toBe(
+      '/workspaces//applications/purple-mermaid-app/pipelineruns/basic-node-js-7c8nd',
+    );
+  });
+
+  it('should render PipelineRunLogs', () => {
+    configure({ testIdAttribute: 'data-testid' });
+    watchResourceMock.mockReturnValue([[pipelineRunMock], true]);
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+
+    screen.getByTestId('logs-tasklist');
+  });
+
+  it('should show empty box if it is not a build pipelinerun', () => {
+    const pipelineruns = [
+      {
+        ...pipelineRunMock,
+        metadata: {
+          ...pipelineRunMock.metadata,
+          labels: {
+            ...pipelineRunMock.metadata.labels,
+            [PipelineRunLabel.PIPELINE_TYPE]: PipelineRunType.TEST,
+          },
+        },
+      },
+    ];
+    watchResourceMock.mockReturnValue([
+      pipelineruns.filter(
+        (p) => p.metadata.labels[PipelineRunLabel.PIPELINE_TYPE] === PipelineRunType.BUILD,
+      ),
+      true,
+    ]);
+
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+    expect(screen.getByTestId('empty-message')).not.toBeNull();
+    expect(screen.getByTestId('empty-message').innerHTML).toBe('No pipeline runs found');
+  });
+
+  it('should show loading box if pipelineRuns are being fetched', () => {
+    watchResourceMock.mockReturnValue([[], false]);
+    useTRPipelineRunsMock.mockReturnValue([[], false]);
+    routerRenderer(<BuildLogViewer component={componentCRMocks[0]} />);
+    expect(screen.getByTestId('loading-indicator')).not.toBeNull();
+  });
+});
---
+++ src/components/Releases/__tests__/ReleasesListView.spec.tsx
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { Table as PfTable, TableHeader } from '@patternfly/react-table/deprecated';
+import { fireEvent, render, screen, within } from '@testing-library/react';
+import { useApplicationReleases } from '../../../hooks/useApplicationReleases';
+import { createUseParamsMock, createUseWorkspaceInfoMock } from '../../../utils/test-utils';
+import { mockReleases } from '../__data__/mock-release-data';
+import ReleasesListRow from '../ReleasesListRow';
+import ReleasesListView from '../ReleasesListView';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: jest.fn(() => ({ t: (x) => x })),
+}));
+
+jest.mock('../../../hooks/useSearchParam', () => ({
+  useSearchParam: () => [...React.useState(''), jest.fn()],
+}));
+
+jest.mock('../../../hooks/useApplicationReleases', () => ({
+  useApplicationReleases: jest.fn(),
+}));
+
+jest.mock('react-router-dom', () => {
+  const actual = jest.requireActual('react-router-dom');
+  return {
+    ...actual,
+    Link: (props) => <a href={props.to}>{props.children}</a>,
+  };
+});
+
+jest.mock('../../../shared/components/table', () => {
+  const actual = jest.requireActual('../../../shared/components/table');
+  return {
+    ...actual,
+    Table: (props) => {
+      const { data, filters, selected, match, kindObj } = props;
+      const cProps = { data, filters, selected, match, kindObj };
+      const columns = props.Header(cProps);
+      return (
+        <PfTable role="table" aria-label="table" cells={columns} variant="compact" borders={false}>
+          <TableHeader role="rowgroup" />
+          <tbody>
+            {props.data.map((obj, i) => (
+              <tr key={i}>
+                <ReleasesListRow
+                  obj={obj}
+                  columns={null}
+                  customData={{ applicationName: 'test-app' }}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </PfTable>
+      );
+    },
+  };
+});
+
+const useMockReleases = useApplicationReleases as jest.Mock;
+
+describe('ReleasesListView', () => {
+  createUseParamsMock({ applicationName: 'test-app' });
+  createUseWorkspaceInfoMock({ workspace: 'test-ws' });
+
+  it('should render progress indicator while loading', async () => {
+    useMockReleases.mockReturnValue([[], false]);
+    const wrapper = render(<ReleasesListView />);
+    expect(await wrapper.findByRole('progressbar')).toBeTruthy();
+  });
+
+  it('should render all columns', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    expect(screen.getByRole('columnheader', { name: 'Name' })).toBeVisible();
+    expect(screen.getByRole('columnheader', { name: 'Created' })).toBeVisible();
+    expect(screen.getByRole('columnheader', { name: 'Status' })).toBeVisible();
+    expect(screen.getByRole('columnheader', { name: 'Release Plan' })).toBeVisible();
+    expect(screen.getByRole('columnheader', { name: 'Release Snapshot' })).toBeVisible();
+  });
+
+  it('should render empty state when no releases present', () => {
+    useMockReleases.mockReturnValue([[], true]);
+    const wrapper = render(<ReleasesListView />);
+    expect(wrapper.findByText('No Releases found')).toBeTruthy();
+  });
+
+  it('should render table', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    const wrapper = render(<ReleasesListView />);
+    const table = wrapper.container.getElementsByTagName('table');
+    expect(table).toHaveLength(1);
+  });
+
+  it('should render filter toolbar', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    const wrapper = render(<ReleasesListView />);
+    screen.getByTestId('filter-toolbar');
+    expect(wrapper.container.getElementsByTagName('table')).toHaveLength(1);
+    expect(wrapper.container.getElementsByTagName('tr')).toHaveLength(4);
+  });
+
+  it('should sort by creation date by default', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    expect(screen.getByRole('columnheader', { name: 'Created' })).toHaveAttribute(
+      'aria-sort',
+      'descending',
+    );
+    expect(screen.getByRole('columnheader', { name: 'Name' })).toHaveAttribute('aria-sort', 'none');
+    const rows = screen.getAllByRole('row');
+    expect(rows[1].children[0]).toHaveTextContent('test-release-2');
+    expect(rows[2].children[0]).toHaveTextContent('test-release');
+    expect(rows[3].children[0]).toHaveTextContent('test-release-3');
+  });
+
+  it('should sort by name on click', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    const table = screen.getByRole('table');
+    fireEvent.click(within(table).getByRole('button', { name: 'Name' }));
+    expect(screen.getByRole('columnheader', { name: 'Name' })).toHaveAttribute(
+      'aria-sort',
+      'ascending',
+    );
+    expect(screen.getByRole('columnheader', { name: 'Created' })).toHaveAttribute(
+      'aria-sort',
+      'none',
+    );
+    const rows = screen.getAllByRole('row');
+    expect(rows[1].children[0]).toHaveTextContent('test-release');
+    expect(rows[2].children[0]).toHaveTextContent('test-release-2');
+    expect(rows[3].children[0]).toHaveTextContent('test-release-3');
+
+    fireEvent.click(within(table).getByRole('button', { name: 'Name' }));
+    expect(screen.getByRole('columnheader', { name: 'Name' })).toHaveAttribute(
+      'aria-sort',
+      'descending',
+    );
+    expect(rows[1].children[0]).toHaveTextContent('test-release-3');
+    expect(rows[2].children[0]).toHaveTextContent('test-release-2');
+    expect(rows[3].children[0]).toHaveTextContent('test-release');
+  });
+
+  it('should allow filtering by name', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    fireEvent.input(screen.getByRole('textbox'), { target: { value: 'test-release-2' } });
+    const rows = screen.getAllByRole('row');
+    expect(rows.length).toBe(2);
+    expect(rows[1].children[0]).toHaveTextContent('test-release-2');
+  });
+
+  it('should allow filtering by release plan', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    fireEvent.click(screen.getAllByRole('button')[0], { name: 'Name' });
+    fireEvent.click(screen.getByRole('option', { name: 'Release plan' }));
+    fireEvent.input(screen.getByRole('textbox'), { target: { value: 'test-plan-2' } });
+    const rows = screen.getAllByRole('row');
+    expect(rows.length).toBe(2);
+    expect(rows[1].children[3]).toHaveTextContent('test-plan-2');
+  });
+
+  it('should allow filtering by release snapshot', () => {
+    useMockReleases.mockReturnValue([mockReleases, true]);
+    render(<ReleasesListView />);
+    fireEvent.click(screen.getAllByRole('button')[0], { name: 'Name' });
+    fireEvent.click(screen.getByRole('option', { name: 'Release snapshot' }));
+    fireEvent.input(screen.getByRole('textbox'), { target: { value: 'test-snapshot-2' } });
+    const rows = screen.getAllByRole('row');
+    expect(rows.length).toBe(2);
+    expect(rows[1].children[4]).toHaveTextContent('test-snapshot-2');
+  });
+});
