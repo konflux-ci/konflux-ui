5d40f3972877d3486d65ad4be142f97c8c9a8419
Cara Wang <wlin@redhat.com>
2025-01-09 17:55:16 +0800

    chore(KFLUXUI-256): support component.spec.containerImage as latestImage

--- src/utils/__tests__/component-utils.spec.ts
+++ src/utils/__tests__/component-utils.spec.ts
@@ -10,6 +10,7 @@ import {
   startNewBuild,
   BUILD_REQUEST_ANNOTATION,
   BuildRequest,
+  getLastestImage,
 } from '../component-utils';
 import { createK8sUtilMock } from '../test-utils';
 
@@ -159,5 +160,38 @@ describe('component-utils', () => {
       pac: { state: 'enabled', 'merge-url': 'example.com' },
       message: 'done',
     });
+  });
+
+  it('should return status.lastPromotedImage when lastPromotedImage is available', () => {
+    const mockComponent = {
+      status: {
+        lastPromotedImage: 'test-url',
+      },
+    } as unknown as ComponentKind;
+
+    expect(getLastestImage(mockComponent)).toEqual('test-url');
+  });
+
+  it('should return spec.containerImage when lastPromotedImage is unavailable', () => {
+    const mockComponent = {
+      spec: {
+        containerImage: 'test-url',
+      },
+    } as unknown as ComponentKind;
+
+    expect(getLastestImage(mockComponent)).toEqual('test-url');
+  });
+
+  it('should return status.lastPromotedImage when lastPromotedImage and containerImage are both available', () => {
+    const mockComponent = {
+      spec: {
+        containerImage: 'test-url',
+      },
+      status: {
+        lastPromotedImage: 'test-url-promoted',
+      },
+    } as unknown as ComponentKind;
+
+    expect(getLastestImage(mockComponent)).toEqual('test-url-promoted');
   });
 });
