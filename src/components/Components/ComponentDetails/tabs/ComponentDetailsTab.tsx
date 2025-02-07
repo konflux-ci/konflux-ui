f5137d21713732c61df10b4018d9c884016ea36f
Joao Pedro Poloni Ponce <jpolonip@redhat.com>
2025-02-07 14:17:49 -0300

    chore: get workspaceName from useParams on ComponentDetailsTab

--- src/components/Components/ComponentDetails/tabs/ComponentDetailsTab.tsx
+++ src/components/Components/ComponentDetails/tabs/ComponentDetailsTab.tsx
@@ -8,24 +8,24 @@ import ComponentNudgesDependencies from '../../../ComponentRelation/details-page
 import { createCustomizeComponentPipelineModalLauncher } from '../../../CustomizedPipeline/CustomizePipelinesModal';
 import { DetailsSection } from '../../../DetailsPage';
 import { useModalLauncher } from '../../../modal/ModalProvider';
-import { useWorkspaceInfo } from '../../../Workspace/useWorkspaceInfo';
+import { useNamespace } from '../../../Namespace/useNamespaceInfo';
 import ComponentBuildSettings from './ComponentBuildSettings';
 import ComponentDetails from './ComponentDetails';
 import ComponentLatestBuild from './ComponentLatestBuild';
 
 const ComponentDetailsTab: React.FC = () => {
-  const { componentName } = useParams<RouterParams>();
-  const { namespace, workspace } = useWorkspaceInfo();
+  const namespace = useNamespace();
+  const { componentName, workspaceName } = useParams<RouterParams>();
   const track = useTrackEvent();
   const showModal = useModalLauncher();
-  const [component] = useComponent(namespace, workspace, componentName);
+  const [component] = useComponent(namespace, workspaceName, componentName);
   const customizePipeline = () => {
     track(TrackEvents.ButtonClicked, {
       link_name: 'manage-build-pipeline',
       link_location: 'component-list-label',
       component_name: component.metadata.name,
       app_name: component.spec.application,
-      workspace,
+      workspace: workspaceName,
     });
     showModal(
       createCustomizeComponentPipelineModalLauncher(
