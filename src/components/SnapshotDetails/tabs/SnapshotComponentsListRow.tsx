05c970534876bfacaea6e1ac6097bf88d361f895
janaki29 <dehankarjanaki@gmail.com>
2025-02-07 15:15:57 +0530

    test(snapshots): test case updtaed for revision as a link

--- src/components/SnapshotDetails/tabs/SnapshotComponentsListRow.tsx
+++ src/components/SnapshotDetails/tabs/SnapshotComponentsListRow.tsx
@@ -38,7 +38,7 @@ const SnapshotComponentsListRow: React.FC<
       <TableData className={commitsTableColumnClasses.revision}>
         <Link
           to={`/workspaces/${workspace}/applications/${obj.application}/commit/${obj.source?.git?.revision}`}
-          // data-test="snapshot-component-revision"
+          data-test="snapshot-component-revision"
         >
           {obj.source?.git?.revision}
         </Link>
