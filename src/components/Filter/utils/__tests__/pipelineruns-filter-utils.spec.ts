16ac328988fff78fa9efc4dd898f2588981c2694
Michal Marcin <marcin.michal02@gmail.com>
2025-02-26 16:24:25 +0100

    chore: add tests for pipelineruns-filter-utils

---
+++ src/components/Filter/utils/__tests__/pipelineruns-filter-utils.spec.ts
@@ -0,0 +1,169 @@
+import { PipelineRunLabel, PipelineRunType } from '../../../../consts/pipelinerun';
+import { PipelineRunKind, PipelineRunStatus } from '../../../../types';
+import { pipelineRunTypes } from '../../../../utils/pipelinerun-utils';
+import { createFilterObj, filterPipelineRuns } from '../pipelineruns-filter-utils';
+
+const pipelineRuns: PipelineRunKind[] = [
+  {
+    kind: 'PipelineRun',
+    apiVersion: 'tekton.dev/v1beta1',
+    metadata: {
+      creationTimestamp: '2022-08-04T16:23:43Z',
+      finalizers: Array['chains.tekton.dev/pipelinerun'],
+      name: 'basic-node-js-first',
+      namespace: 'test',
+      ownerReferences: [
+        {
+          apiVersion: 'appstudio.redhat.com/v1alpha1',
+          kind: 'Component',
+          name: 'basic-node-js',
+          uid: '6b79df0c-1bee-40c0-81ee-7c4d1c9a422f',
+        },
+      ],
+      resourceVersion: '497868251',
+      uid: '9c1f121c-1eb6-490f-b2d9-befbfc658df1',
+      labels: {
+        'appstudio.openshift.io/component': 'sample-component',
+        [PipelineRunLabel.PIPELINE_TYPE]: PipelineRunType.TEST,
+      },
+    },
+    spec: {
+      key: 'key1',
+    },
+    status: {
+      conditions: [
+        {
+          status: 'True',
+          type: 'Succeeded',
+        },
+      ],
+    } as PipelineRunStatus,
+  },
+  {
+    kind: 'PipelineRun',
+    apiVersion: 'tekton.dev/v1beta1',
+    metadata: {
+      creationTimestamp: '2022-08-04T16:23:43Z',
+      finalizers: Array['chains.tekton.dev/pipelinerun'],
+      name: 'basic-node-js-second',
+      namespace: 'test',
+      ownerReferences: [
+        {
+          apiVersion: 'appstudio.redhat.com/v1alpha1',
+          kind: 'Component',
+          name: 'basic-node-js',
+          uid: '6b79df0c-1bee-40c0-81ee-7c4d1c9a422f',
+        },
+      ],
+      resourceVersion: '497868252',
+      uid: '9c1f121c-1eb6-490f-b2d9-befbfc658dfb',
+      labels: {
+        'appstudio.openshift.io/component': 'test-component',
+        [PipelineRunLabel.PIPELINE_TYPE]: PipelineRunType.BUILD,
+      },
+    },
+    spec: {
+      key: 'key2',
+    },
+  },
+  {
+    kind: 'PipelineRun',
+    apiVersion: 'tekton.dev/v1beta1',
+    metadata: {
+      creationTimestamp: '2022-08-04T16:23:43Z',
+      finalizers: Array['chains.tekton.dev/pipelinerun'],
+      name: 'basic-node-js-third',
+      namespace: 'test',
+      ownerReferences: [
+        {
+          apiVersion: 'appstudio.redhat.com/v1alpha1',
+          kind: 'Component',
+          name: 'basic-node-js',
+          uid: '6b79df0c-1bee-40c0-81ee-7c4d1c9a422f',
+        },
+      ],
+      resourceVersion: '497868253',
+      uid: '9c1f121c-1eb6-490f-b2d9-befbfc658dfc',
+      labels: {
+        'appstudio.openshift.io/component': 'sample-component',
+        [PipelineRunLabel.PIPELINE_TYPE]: PipelineRunType.BUILD,
+      },
+    },
+    spec: {
+      key: 'key3',
+    },
+  },
+];
+
+describe('pipelineruns-filter-utils', () => {
+  describe('filterPipelineRuns', () => {
+    it('should filter pipeline runs by name', () => {
+      const filters = {
+        nameFilter: 'basic-node-js-first',
+        statusFilter: [],
+        typeFilter: [],
+      };
+      const result = filterPipelineRuns(pipelineRuns, filters);
+      expect(result.length).toBe(1);
+      expect(result[0].metadata.name).toBe('basic-node-js-first');
+    });
+
+    it('should filter pipeline runs by status', () => {
+      const filters = {
+        nameFilter: '',
+        statusFilter: ['Succeeded'],
+        typeFilter: [],
+      };
+      const result = filterPipelineRuns(pipelineRuns, filters);
+      expect(result.length).toBe(1);
+      expect(result[0].metadata.name).toBe('basic-node-js-first');
+    });
+
+    it('should filter pipeline runs by type', () => {
+      const filters = {
+        nameFilter: '',
+        statusFilter: [],
+        typeFilter: ['build'],
+      };
+      const expectedNames = ['basic-node-js-second', 'basic-node-js-third'];
+
+      const result = filterPipelineRuns(pipelineRuns, filters);
+      expect(result.length).toBe(2);
+      const resultNames = [result[0].metadata.name, result[1].metadata.name];
+      expect(resultNames.sort()).toStrictEqual(expectedNames.sort());
+    });
+
+    it('should filter pipeline runs by custom filter', () => {
+      const filters = {
+        nameFilter: '',
+        statusFilter: [],
+        typeFilter: [],
+      };
+      const expectedNames = ['basic-node-js-first', 'basic-node-js-third'];
+      const customFilter = (plr: PipelineRunKind) =>
+        plr.metadata.labels['appstudio.openshift.io/component'] === 'sample-component';
+
+      const result = filterPipelineRuns(pipelineRuns, filters, customFilter);
+      expect(result.length).toBe(2);
+      const resultNames = [result[0].metadata.name, result[1].metadata.name];
+      expect(resultNames.sort()).toStrictEqual(expectedNames.sort());
+    });
+  });
+
+  describe('createFilterObj', () => {
+    it('should count pipelinerun keys for filter options', () => {
+      const result = createFilterObj(
+        pipelineRuns,
+        (plr) => plr?.metadata.labels[PipelineRunLabel.COMMIT_TYPE_LABEL],
+        pipelineRunTypes,
+        (plr: PipelineRunKind) => plr.kind === 'PipelineRun',
+      );
+      const expected = {
+        build: 2,
+        test: 1,
+      };
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
