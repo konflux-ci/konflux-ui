56e15278806395eda6554418249827a7ccc1ddb0
StanleyJochman <sjochman@redhat.com>
2025-03-11 10:16:38 +0100

    fix(KFLUXUI-298): rebasing into Cara's main

--- src/hooks/usePipelineRuns.ts
+++ src/hooks/usePipelineRuns.ts
@@ -22,7 +22,6 @@ const useRuns = <Kind extends K8sResourceCommon>(
   groupVersionKind: K8sGroupVersionKind,
   model: K8sModelCommon,
   namespace: string,
-  workspace,
   options?: {
     selector?: Selector;
     limit?: number;
