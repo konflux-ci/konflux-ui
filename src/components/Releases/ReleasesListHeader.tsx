6935d9e89c9aacb35c7af57f0438de154e313bf3
Ralph Bean <rbean@redhat.com>
2025-03-13 16:54:31 -0400

    feat: display links to tenant, managed, and final releases pipelines

--- src/components/Releases/ReleasesListHeader.tsx
+++ src/components/Releases/ReleasesListHeader.tsx
@@ -2,12 +2,15 @@ import { SortByDirection, ThProps } from '@patternfly/react-table';
 import { HeaderFunc } from '../../shared/components/table/Table';
 
 export const releasesTableColumnClasses = {
-  name: 'pf-m-width-30 pf-m-width-20-on-xl wrap-column',
-  created: 'pf-m-width-20 pf-m-width-20-on-xl',
-  duration: 'pf-m-hidden pf-m-width-20 pf-m-visible-on-xl',
-  status: 'pf-m-width-20',
-  releasePlan: 'pf-m-width-25',
-  releaseSnapshot: 'pf-m-hidden pf-m-width-25 pf-m-visible-on-xl',
+  name: 'pf-m-width-20  pf-m-width-10-on-xl wrap-column',
+  created: 'pf-m-width-20  pf-m-width-15-on-xl',
+  duration: 'pf-m-width-20  pf-m-width-15-on-xl',
+  status: 'pf-m-width-20  pf-m-width-10-on-xl',
+  releasePlan: 'pf-m-width-20  pf-m-width-10-on-xl',
+  releaseSnapshot: 'pf-m-hidden    pf-m-width-10-on-xl pf-m-visible-on-xl',
+  managedPipelineRun: 'pf-m-hidden    pf-m-width-10-on-xl pf-m-visible-on-xl',
+  tenantPipelineRun: 'pf-m-hidden    pf-m-width-10-on-xl pf-m-visible-on-xl',
+  finalPipelineRun: 'pf-m-hidden    pf-m-width-10-on-xl pf-m-visible-on-xl',
   kebab: 'pf-v5-c-table__action',
 };
 
@@ -59,6 +62,18 @@ const getReleasesListHeader: CreateHeader = (activeIndex, activeDirection, onSor
     {
       title: 'Release Snapshot',
       props: { className: releasesTableColumnClasses.releaseSnapshot },
+    },
+    {
+      title: 'Tenant Pipeline',
+      props: { className: releasesTableColumnClasses.tenantPipelineRun },
+    },
+    {
+      title: 'Managed Pipeline',
+      props: { className: releasesTableColumnClasses.managedPipelineRun },
+    },
+    {
+      title: 'Final Pipeline',
+      props: { className: releasesTableColumnClasses.finalPipelineRun },
     },
     {
       title: ' ',
