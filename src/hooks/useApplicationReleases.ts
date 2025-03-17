58956f0e0daec693cacb6fb3e2b245f2e527f044
Sahil Budhwar <sahilbudhwar143@gmail.com>
2025-03-17 15:47:32 +0530

    fix: fetch releases based on the metadata.labels

--- src/hooks/useApplicationReleases.ts
+++ src/hooks/useApplicationReleases.ts
@@ -1,37 +1,10 @@
-import * as React from 'react';
-import { useK8sWatchResource } from '../k8s';
-import { ReleaseGroupVersionKind, ReleaseModel } from '../models';
 import { useNamespace } from '../shared/providers/Namespace';
 import { ReleaseKind } from '../types';
-import { useApplicationSnapshots } from './useApplicationSnapshots';
+import { useReleases } from './useReleases';
 
 export const useApplicationReleases = (
   applicationName: string,
 ): [ReleaseKind[], boolean, unknown] => {
   const namespace = useNamespace();
-  const {
-    data: releases,
-    isLoading: releasesLoaded,
-    error: releasesError,
-  } = useK8sWatchResource<ReleaseKind[]>(
-    {
-      groupVersionKind: ReleaseGroupVersionKind,
-      namespace,
-      isList: true,
-      watch: true,
-    },
-    ReleaseModel,
-  );
-
-  const [snapshots, snapshotsLoaded, snapshotsError] = useApplicationSnapshots(applicationName);
-
-  const releasesForApp = React.useMemo(
-    () =>
-      !releasesLoaded && snapshotsLoaded
-        ? releases.filter((r) => snapshots.some((s) => s.metadata.name === r.spec.snapshot))
-        : [],
-    [releases, releasesLoaded, snapshots, snapshotsLoaded],
-  );
-
-  return [releasesForApp, !releasesLoaded && snapshotsLoaded, releasesError || snapshotsError];
+  return useReleases(namespace, applicationName);
 };
