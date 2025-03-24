eba8c83aa67ccb0e57831263a015a5b85df34b80
Abhi <abhinandan13jan@gmail.com>
2025-03-24 18:51:48 +0530

    fix(release): use displayName instead of release.spec.application

--- src/components/ReleaseService/ReleasePlan/__tests__/ReleasePlanListRow.spec.tsx
+++ src/components/ReleaseService/ReleasePlan/__tests__/ReleasePlanListRow.spec.tsx
@@ -1,5 +1,9 @@
 import { render } from '@testing-library/react';
-import { mockReleasePlan } from '../__data__/release-plan.mock';
+import {
+  mockApplicationWithDisplayName,
+  mockApplicationWithoutDisplayName,
+  mockReleasePlan,
+} from '../__data__/release-plan.mock';
 import ReleasePlanListRow from '../ReleasePlanListRow';
 
 jest.mock('react-router-dom', () => ({
@@ -8,9 +12,40 @@ jest.mock('react-router-dom', () => ({
 
 describe('ReleasePlanListRow', () => {
   it('should render release plan data into table cells', () => {
-    const wrapper = render(<ReleasePlanListRow obj={mockReleasePlan} columns={[]} />, {
-      container: document.createElement('tr'),
-    });
+    const wrapper = render(
+      <ReleasePlanListRow
+        obj={{
+          ...mockReleasePlan,
+          application: mockApplicationWithDisplayName,
+        }}
+        columns={[]}
+      />,
+      {
+        container: document.createElement('tr'),
+      },
+    );
+    const cells = wrapper.container.getElementsByTagName('td');
+
+    expect(cells[0].innerHTML).toBe(mockReleasePlan.metadata.name);
+    expect(cells[1].children[0].innerHTML).toBe(mockApplicationWithDisplayName.spec.displayName);
+    expect(cells[2].innerHTML).toBe('rorai-tenant');
+    expect(cells[3].innerHTML).toBe('True');
+    expect(cells[4].innerHTML).toBe('True');
+  });
+
+  it('should fallback to application metadata name when displayName not there', () => {
+    const wrapper = render(
+      <ReleasePlanListRow
+        obj={{
+          ...mockReleasePlan,
+          application: mockApplicationWithoutDisplayName,
+        }}
+        columns={[]}
+      />,
+      {
+        container: document.createElement('tr'),
+      },
+    );
     const cells = wrapper.container.getElementsByTagName('td');
 
     expect(cells[0].innerHTML).toBe(mockReleasePlan.metadata.name);
