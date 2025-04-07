cd123f34e816d88b185bb8576e424e32eefde77d
Abhi <abhinandan13jan@gmail.com>
2025-04-07 16:54:05 +0530

    feat(release): application name  contistency

--- src/components/Applications/application-utils.ts
+++
@@ -1,4 +0,0 @@
-import { ApplicationKind } from '~/types';
-
-export const getApplicationDisplayName = (application: ApplicationKind): string =>
-  application?.spec?.displayName ?? application?.metadata?.name;
