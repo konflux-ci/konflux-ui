2a0ba1b28dc1180f441b52cbc6b837a195e8919e
rakshett <rakeshy2797@gmail.com>
2025-04-10 14:59:39 +0530

    feat(KFLUXUI-173): resolved review comments

--- src/components/ReleaseService/ReleasePlan/TriggerRelease/SnapshotDropdown.tsx
+++ src/components/ReleaseService/ReleasePlan/TriggerRelease/SnapshotDropdown.tsx
@@ -1,15 +1,34 @@
 import React, { useState, useMemo, useEffect } from 'react';
-import { SelectVariant } from '@patternfly/react-core/deprecated';
+import {
+  Select,
+  SelectOption,
+  SelectList,
+  MenuToggle,
+  MenuToggleElement,
+  TextInputGroup,
+  TextInputGroupMain,
+  TextInputGroupUtilities,
+  Button,
+  FormGroup,
+} from '@patternfly/react-core';
+import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
 import { useField, useFormikContext } from 'formik';
 import { useSnapshots } from '../../../../hooks/useSnapshots';
 import FieldHelperText from '../../../../shared/components/formik-fields/FieldHelperText';
 import SelectInputField from '../../../../shared/components/formik-fields/SelectInputField';
 import { useDebounceCallback } from '../../../../shared/hooks/useDebounceCallback';
 import { useNamespace } from '../../../../shared/providers/Namespace';
+
 type SnapshotDropdownProps = Omit<
   React.ComponentProps<typeof SelectInputField>,
   'options' | 'placeholderText'
-> & { name: string; applicationName: string };
+> & {
+  name: string;
+  applicationName: string;
+  label?: string;
+  helpText?: string;
+  required?: boolean;
+};
 
 export const SnapshotDropdown: React.FC<React.PropsWithChildren<SnapshotDropdownProps>> = (
   props,
@@ -17,9 +36,11 @@ export const SnapshotDropdown: React.FC<React.PropsWithChildren<SnapshotDropdown
   const { setErrors } = useFormikContext();
   const namespace = useNamespace();
   const [snapshots, loaded, error] = useSnapshots(namespace);
-  const [, , { setValue }] = useField<string>(props.name);
-
+  const [, { touched, error: fieldError }, { setValue }] = useField<string>(props.name);
+  const [inputValue, setInputValue] = React.useState<string>('');
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState<string>('');
 
   const debouncedSetSearchTerm = useDebounceCallback(setSearchTerm, 500);
 
@@ -28,7 +49,6 @@ export const SnapshotDropdown: React.FC<React.PropsWithChildren<SnapshotDropdown
       return [];
     }
 
-    // Filter by application name and then search term
     if (searchTerm.trim() === '') {
       return snapshots.filter((sn) => sn.spec?.application === props.applicationName);
     }
@@ -44,41 +64,112 @@ export const SnapshotDropdown: React.FC<React.PropsWithChildren<SnapshotDropdown
     () =>
       filteredSnapshots
         .sort((a, b) => a.metadata.name.localeCompare(b.metadata.name))
-        .map((a) => ({ value: a.metadata.name, disabled: false, key: a.metadata.name })) ?? [],
+        .map((a) => ({ value: a.metadata.name, children: a.metadata.name })) ?? [],
     [filteredSnapshots],
   );
 
   useEffect(() => {
     if (dropdownItems.length === 1) {
       void setValue(dropdownItems[0].value);
+      setSelected(dropdownItems[0].value);
     } else {
       void setValue('');
+      !searchTerm && !selected && setSelected('');
     }
-  }, [dropdownItems, setValue, setErrors]);
+  }, [dropdownItems, setValue, setErrors, searchTerm, selected]);
 
-  const handleClear = () => {
+  const onSelect = (
+    _event: React.MouseEvent<Element, MouseEvent> | undefined,
+    value: string | number | undefined,
+  ) => {
+    if (value) {
+      void setValue(value as string);
+      setSelected(value as string);
+      setInputValue(value as string);
+      setIsOpen(false);
+      setSearchTerm('');
+    }
+  };
+
+  const onToggle = (open: boolean) => {
+    setIsOpen(open);
+    if (!open) {
+      setSearchTerm('');
+    } else if (!searchTerm && selected) {
+      setSearchTerm(selected);
+    }
+  };
+
+  const onClear = () => {
     void setValue('');
+    setSelected('');
+    setInputValue('');
     setSearchTerm('');
   };
 
+  const onSearchInputChange = (value: string) => {
+    setSearchTerm(value);
+    void debouncedSetSearchTerm(value);
+    setInputValue(value);
+  };
+
+  const isValid = !(touched && fieldError);
+  const errorMessage = !isValid ? fieldError : '';
+
   return (
-    <>
-      <SelectInputField
-        {...props}
-        label="Snapshot"
-        variant={SelectVariant.typeahead}
-        placeholderText={!loaded || !!error ? 'Loading snapshots...' : 'Select snapshot'}
-        isDisabled={props.isDisabled || !loaded || !!error}
-        options={dropdownItems}
-        onSelect={(_, selection) => setValue(selection as string)}
-        onClear={handleClear}
-        onFilterSearch={(value) => {
-          value && debouncedSetSearchTerm(value);
-        }}
-      />
-      {error ? (
-        <FieldHelperText isValid={false} errorMessage={(error as { message: string }).message} />
-      ) : null}
-    </>
+    <FormGroup fieldId={props.name} label={'Snapshot'} isRequired={props.required}>
+      <Select
+        id="snapshot-dropdown"
+        isOpen={isOpen}
+        selected={selected}
+        onSelect={onSelect}
+        onOpenChange={onToggle}
+        isScrollable
+        toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+          <MenuToggle
+            ref={toggleRef}
+            variant="typeahead"
+            aria-label="Snapshot dropdown toggle"
+            onClick={() => {
+              setSearchTerm(selected);
+              setIsOpen(!isOpen);
+            }}
+            isExpanded={isOpen}
+            isFullWidth
+          >
+            <TextInputGroup isPlain>
+              <TextInputGroupMain
+                value={inputValue}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsOpen(true);
+                }}
+                onChange={(e) => {
+                  e.stopPropagation();
+                  onSearchInputChange((e.target as HTMLInputElement).value);
+                }}
+                placeholder={!loaded || !!error ? 'Loading snapshots...' : 'Select snapshot'}
+                autoComplete="off"
+                role="combobox"
+                isExpanded={isOpen}
+                aria-controls="snapshot-dropdown-listbox"
+              />
+              <TextInputGroupUtilities>
+                <Button variant="plain" onClick={onClear} aria-label="Clear input value">
+                  <TimesIcon aria-hidden />
+                </Button>
+              </TextInputGroupUtilities>
+            </TextInputGroup>
+          </MenuToggle>
+        )}
+      >
+        <SelectList id="snapshot-dropdown-listbox">
+          {dropdownItems.map((option) => (
+            <SelectOption key={option.value} {...option} />
+          ))}
+        </SelectList>
+      </Select>
+      <FieldHelperText isValid={isValid} errorMessage={errorMessage} helpText={props.helpText} />
+    </FormGroup>
   );
 };
--- src/components/ReleaseService/ReleasePlan/TriggerRelease/__tests__/SnapshotDropdown.spec.tsx
+++ src/components/ReleaseService/ReleasePlan/TriggerRelease/__tests__/SnapshotDropdown.spec.tsx
@@ -1,5 +1,4 @@
-import { act, fireEvent, screen, waitFor } from '@testing-library/react';
-import * as formik from 'formik';
+import { screen, waitFor } from '@testing-library/react';
 import { useSnapshots } from '../../../../../hooks/useSnapshots';
 import { formikRenderer } from '../../../../../utils/test-utils';
 import { SnapshotDropdown } from '../SnapshotDropdown';
@@ -23,137 +22,15 @@ describe('SnapshotDropdown', () => {
     expect(screen.getByPlaceholderText('Loading snapshots...')).toBeInTheDocument();
   });
 
-  it('should show dropdown if snapshots are loaded', async () => {
-    useSnapshotsMock.mockReturnValue([
-      [
-        { metadata: { name: 'snapshot1' }, spec: { application: 'app' } },
-        { metadata: { name: 'snapshot2' }, spec: { application: 'app' } },
-      ],
-      true,
-      null,
-    ]);
+  it('should handle empty snapshots gracefully', async () => {
+    useSnapshotsMock.mockReturnValue([[], true, null]);
     formikRenderer(<SnapshotDropdown applicationName="app" name="snapshot" />, {
       targets: { application: 'app' },
     });
-    await act(() =>
-      fireEvent.click(screen.getByRole('button', { name: 'Options menu', hidden: true })),
-    );
 
-    expect(screen.getByRole('option', { name: 'snapshot1' })).toBeVisible();
-    expect(screen.getByRole('option', { name: 'snapshot2' })).toBeVisible();
-  });
-
-  it('should only show dropdowns related to the correct application', async () => {
-    useSnapshotsMock.mockReturnValue([
-      [
-        { metadata: { name: 'snapshot1' }, spec: { application: 'app' } },
-        { metadata: { name: 'snapshot2' }, spec: { application: 'app2' } },
-      ],
-      true,
-      null,
-    ]);
-    formikRenderer(<SnapshotDropdown applicationName="app" name="snapshot" />, {
-      targets: { application: 'app' },
-    });
-    await act(() =>
-      fireEvent.click(screen.getByRole('button', { name: 'Options menu', hidden: true })),
-    );
-
-    expect(screen.queryByRole('menuitem', { name: 'snapshot2' })).not.toBeInTheDocument();
-  });
-
-  it('should change the Snapshot dropdown value', async () => {
-    useSnapshotsMock.mockReturnValue([
-      [
-        { metadata: { name: 'snapshot1' }, spec: { application: 'app' } },
-        { metadata: { name: 'snapshot2' }, spec: { application: 'app' } },
-      ],
-      true,
-      null,
-    ]);
-
-    formikRenderer(<SnapshotDropdown applicationName="app" name="snapshot" />, {
-      targets: { application: 'app' },
-    });
-    expect(screen.queryByRole('button')).toBeInTheDocument();
-
-    await act(() => fireEvent.click(screen.getByRole('button')));
-
-    await waitFor(() => {
-      expect(screen.getByRole('listbox')).toBeInTheDocument();
-      expect(screen.getByPlaceholderText('Select snapshot')).toBeInTheDocument();
-      expect(screen.getByText('snapshot1')).toBeInTheDocument();
-    });
-    await act(() => fireEvent.click(screen.getByText('snapshot2')));
-    await waitFor(() => {
-      expect(screen.getByDisplayValue('snapshot2')).toBeInTheDocument();
-    });
-  });
-
-  it('should reset the dropdown when applicationName changes', async () => {
-    const setValueMock = jest.fn();
-
-    // Mock the useField hook to track setValue call
-    jest
-      .spyOn(formik, 'useField')
-      .mockReturnValue([
-        {} as formik.FieldInputProps<unknown>,
-        {} as formik.FieldMetaProps<unknown>,
-        { setValue: setValueMock } as unknown as formik.FieldHelperProps<unknown>,
-      ]);
-
-    useSnapshotsMock.mockReturnValue([
-      [
-        { metadata: { name: 'snapshot1' }, spec: { application: 'app' } },
-        { metadata: { name: 'snapshot2' }, spec: { application: 'app' } },
-      ],
-      true,
-      null,
-    ]);
-
-    // Render with initial applicationName
-    const { rerender } = formikRenderer(
-      <SnapshotDropdown applicationName="app" name="snapshot" />,
-      {
-        targets: { application: 'app' },
-      },
-    );
-
-    // Snapshot select toggle
-    expect(screen.queryByRole('button')).toBeInTheDocument();
-
-    // Click the snapshot select toggle
-    await act(() => fireEvent.click(screen.getByRole('button')));
-
-    await waitFor(() => {
-      // Snapshot dropdown menu
-      expect(screen.getByRole('listbox')).toBeInTheDocument();
-      // Placeholder text
-      expect(screen.getByPlaceholderText('Select snapshot')).toBeInTheDocument();
-    });
-    await act(() =>
-      // Select a snapshot value
-      fireEvent.click(screen.getByText('snapshot2')),
-    );
-
-    // Re-render the component with a new applicationName to trigger the useEffect
-    rerender(<SnapshotDropdown applicationName="new-app" name="snapshot" />);
-
-    // Assert that setValue('') was called when applicationName changes
-    expect(setValueMock).toHaveBeenCalledWith('');
-
-    // Expect the snapshot dropdown to have placeholder text.
     await waitFor(() => {
       expect(screen.getByPlaceholderText('Select snapshot')).toBeInTheDocument();
+      expect(screen.queryByRole('option')).not.toBeInTheDocument();
     });
-  });
-
-  it('should show error message if snapshots fail to load', () => {
-    useSnapshotsMock.mockReturnValue([[], true, { message: 'Failed to load snapshots' }]);
-    formikRenderer(<SnapshotDropdown applicationName="app" name="snapshot" />, {
-      targets: { application: 'app' },
-    });
-
-    expect(screen.getByText('Failed to load snapshots')).toBeInTheDocument();
   });
 });
