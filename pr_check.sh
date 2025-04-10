e18bc6d8f15b0ab4e527f71c37ac42786b96cced
Pavel Sturc <psturc@redhat.com>
2025-04-10 17:16:27 +0200

    test: use Sealights

--- pr_check.sh
+++ pr_check.sh
@@ -1,87 +1,148 @@
 #!/bin/bash
 
-# -------------------------------------------
-# ----------- RUN UI WITH CHANGES -----------
-# -------------------------------------------
-echo "Running UI from commit sha ${HEAD_SHA}"
-cd konflux-ui
-./connect_to_local_konflux.sh
-yarn install
+export NODEJS_AGENT_IMAGE=quay.io/konflux-ci/tekton-integration-catalog/sealights-nodejs:latest
+script_path="$(dirname -- "${BASH_SOURCE[0]}")"
 
-# start the UI from the PR check in background, save logs to file
-yarn start > yarn_start_logfile 2>&1 &
-YARN_PID=$!
+build_ui_image() {
+    set -euo pipefail
+    
+    echo "Building UI from commit sha ${HEAD_SHA}"
+    
+    export IMAGE_NAME=localhost/test/test
+    export IMAGE_TAG=konflux-ui
+    export KONFLUX_UI_IMAGE_REF=${IMAGE_NAME}:${IMAGE_TAG}
+    export TARGET_BRANCH=${TARGET_BRANCH##*/}
 
-# monitor memory usage during a test
-while true; do date '+%F_%H:%M:%S' >> mem.log && free -m >> mem.log; sleep 1; done 2>&1 &
-MEM_PID=$!
+    # Update konflux-ui image name and tag in konflux-ci kustomize files
+    local ui_kustomize_yaml_path="${script_path}/konflux-ci/konflux-ci/ui/core/kustomization.yaml"
+    yq eval --inplace "del(.images[] | select(.name == \"*konflux-ui*\") | .digest)" "${ui_kustomize_yaml_path}"
+    yq eval --inplace "(.images[] | select(.name == \"*konflux-ui*\")) |=.newTag=\"${IMAGE_TAG}\"" "${ui_kustomize_yaml_path}"
+    yq eval --inplace "(.images[] | select(.name == \"*konflux-ui*\")) |=.newName=\"${IMAGE_NAME}\"" "${ui_kustomize_yaml_path}"
 
-set -x
+    # TODO: change to konflux-ui
+    export COMPONENT=nodejs-test
+    export AGENT_VERSION
 
-# -------------------------------------
-# ----------- RUN E2E TESTS -----------
-# -------------------------------------
+    AGENT_VERSION=$(podman run $NODEJS_AGENT_IMAGE /bin/sh -c 'echo ${AGENT_VERSION}')
 
-# default image used if test code is not changed ina PR
-TEST_IMAGE="quay.io/konflux_ui_qe/konflux-ui-tests:latest"
+    if [ "${JOB_TYPE}" == "on-pr" ]; then
+    podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+        $NODEJS_AGENT_IMAGE \
+        /bin/bash -cx "slnodejs prConfig --appName ${COMPONENT} --targetBranch ${TARGET_BRANCH} --repositoryUrl ${FORKED_REPO_URL} --latestCommit ${HEAD_SHA} --pullRequestNumber ${PR_NUMBER} --token ${SEALIGHTS_TOKEN}"
+    elif [ "${JOB_TYPE}" == "periodic" ]; then
+    podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+        $NODEJS_AGENT_IMAGE \
+        /bin/bash -cx "slnodejs config --appName ${COMPONENT} --branch ${TARGET_BRANCH} --build ${HEAD_SHA} --token ${SEALIGHTS_TOKEN}"
+    else
+        echo "ERROR: invalid job type '${JOB_TYPE}' specified"
+    fi
 
-# fetch also target branch
-git fetch origin ${TARGET_BRANCH}
+    echo "$SEALIGHTS_TOKEN" > /tmp/sl-token
 
-# Rebuild test image if Containerfile or entrypoint from e2e-tests was changed 
-git diff --exit-code --quiet origin/${TARGET_BRANCH} HEAD -- e2e-tests/Containerfile || is_changed_cf=$?
-git diff --exit-code --quiet origin/${TARGET_BRANCH} HEAD -- e2e-tests/entrypoint.sh || is_changed_ep=$?
+    podman build --build-arg BSID="$(< buildSessionId)" \
+        --build-arg AGENT_VERSION="${AGENT_VERSION}" \
+        --secret id=sealights-credentials/token,src=/tmp/sl-token \
+        -t ${KONFLUX_UI_IMAGE_REF} \
+        -f Dockerfile.sealights .
 
-if [[ ($is_changed_cf -eq 1) || ($is_changed_ep -eq 1) ]]; then
-    echo "Containerfile changes detected, rebuilding test image"
-    TEST_IMAGE="konflux-ui-tests:pr-$PR_NUMBER"
+    podman image save -o konflux-ui.tar ${KONFLUX_UI_IMAGE_REF}
+    kind load image-archive konflux-ui.tar -n konflux
+}
 
-    cd e2e-tests
-    podman build -t "$TEST_IMAGE" . -f Containerfile
-    cd ..
-else 
-    echo "Using latest image from quay."
+
+run_test() {
+    # default image used if test code is not changed in a PR
+    TEST_IMAGE="quay.io/konflux_ui_qe/konflux-ui-tests:latest"
+
+    # monitor memory usage during a test
+    while true; do date '+%F_%H:%M:%S' >> mem.log && free -m >> mem.log; sleep 1; done 2>&1 &
+    MEM_PID=$!
+
+    # fetch also target branch
+    git fetch origin "${TARGET_BRANCH}"
+
+    # Rebuild test image if Containerfile or entrypoint from e2e-tests was changed 
+    git diff --exit-code --quiet "origin/${TARGET_BRANCH}" HEAD -- e2e-tests/Containerfile || is_changed_cf=$?
+    git diff --exit-code --quiet "origin/${TARGET_BRANCH}" HEAD -- e2e-tests/entrypoint.sh || is_changed_ep=$?
+
+    if [[ ($is_changed_cf -eq 1) || ($is_changed_ep -eq 1) ]]; then
+        echo "Containerfile changes detected, rebuilding test image"
+        TEST_IMAGE="konflux-ui-tests:pr-$PR_NUMBER"
+
+        cd e2e-tests
+        podman build -t "$TEST_IMAGE" . -f Containerfile
+        cd ..
+    else 
+        echo "Using latest image from quay."
+    fi
+    mkdir artifacts
+    echo "running tests using image ${TEST_IMAGE}"
+    COMMON_SETUP="-v $PWD/artifacts:/tmp/artifacts:Z,U \
+        -v $PWD/e2e-tests:/e2e:Z,U \
+        --timeout=3600 \
+        -e CYPRESS_PR_CHECK=true \
+        -e CYPRESS_KONFLUX_BASE_URL=https://localhost:9443 \
+        -e CYPRESS_USERNAME=${CYPRESS_USERNAME} \
+        -e CYPRESS_PASSWORD=${CYPRESS_PASSWORD} \
+        -e CYPRESS_GH_TOKEN=${CYPRESS_GH_TOKEN}"
+
+
+    podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+        $NODEJS_AGENT_IMAGE \
+        /bin/bash -cx "slnodejs start --teststage konflux-ui-e2e --buildsessionidfile buildSessionId --token ${SEALIGHTS_TOKEN}"
+
+    TEST_RUN=0
+    set +e
+    podman run --network host ${COMMON_SETUP} ${TEST_IMAGE}
+    PODMAN_RETURN_CODE=$?
+    if [[ $PODMAN_RETURN_CODE -ne 0 ]]; then
+        case $PODMAN_RETURN_CODE in
+            255)
+                echo "Test took too long, podman exited due to timeout set to 1 hour."
+                ;;
+            130)
+                echo "Podman run was interrupted."
+                ;;
+            *)
+                echo "Podman exited with exit code: ${PODMAN_RETURN_CODE}"
+                ;;
+        esac
+        TEST_RUN=1
+    fi
+
+    podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+        $NODEJS_AGENT_IMAGE \
+        /bin/bash -cx "slnodejs end --buildsessionidfile buildSessionId --token ${SEALIGHTS_TOKEN}"
+
+    kubectl logs "$(kubectl get pods -n konflux-ui -o name | grep proxy)" --all-containers=true -n konflux-ui > "$PWD/artifacts/konflux-ui.log"
+
+    # kill the background process monitoring memory usage
+    kill $MEM_PID
+    cp mem.log "$PWD/artifacts"
+
+    echo "Exiting pr_check.sh with code $TEST_RUN"
+
+    exit $TEST_RUN
+}
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [build|test]"
+    exit 1
 fi
-mkdir artifacts
-echo "running tests using image ${TEST_IMAGE}"
-COMMON_SETUP="-v $PWD/artifacts:/tmp/artifacts:Z,U \
-    -v $PWD/e2e-tests:/e2e:Z,U \
-    --timeout=3600 \
-    -e CYPRESS_PR_CHECK=true \
-    -e CYPRESS_KONFLUX_BASE_URL=https://localhost:8080 \
-    -e CYPRESS_USERNAME=${CYPRESS_USERNAME} \
-    -e CYPRESS_PASSWORD=${CYPRESS_PASSWORD} \
-    -e CYPRESS_GH_TOKEN=${CYPRESS_GH_TOKEN}"
 
-TEST_RUN=0
-set +e
-podman run --network host ${COMMON_SETUP} ${TEST_IMAGE} 
-PODMAN_RETURN_CODE=$?
-if [[ $PODMAN_RETURN_CODE -ne 0 ]]; then
-    case $PODMAN_RETURN_CODE in
-        255)
-            echo "Test took too long, podman exited due to timeout set to 1 hour."
-            ;;
-        130)
-            echo "Podman run was interrupted."
-            ;;
-        *)
-            echo "Podman exited with exit code: ${PODMAN_RETURN_CODE}"
-            ;;
-    esac
-    TEST_RUN=1
-fi
-
-# kill the background process running the UI
-kill $YARN_PID
-cp yarn_start_logfile $PWD/artifacts
-
-# kill the background process monitoring memory usage
-kill $MEM_PID
-cp mem.log $PWD/artifacts
-
-echo "Exiting pr_check.sh with code $TEST_RUN"
-
-cd ..
-exit $TEST_RUN
+case "$1" in
+    build)
+        echo "Running build process..."
+        build_ui_image
+        ;;
+    test)
+        echo "Running test suite..."
+        run_test
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        echo "Usage: $0 [build|test]"
+        exit 1
+        ;;
+esac
 
