d1d53d82e24f26ba2d45c80673c15314c7a83200
Cara Wang <wlin@redhat.com>
2025-04-10 21:48:11 +0800

    feat(KONFLUX-5817): enjoy the virtualized table for ecResults

--- src/components/EnterpriseContract/EnterpriseContractTable/__tests__/EnterpriseContractRow.spec.tsx
+++ src/components/EnterpriseContract/EnterpriseContractTable/__tests__/EnterpriseContractRow.spec.tsx
@@ -1,7 +1,8 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { UIEnterpriseContractData } from '~/types';
 import { mockUseNamespaceHook } from '~/unit-test-utils/mock-namespace';
 import { ENTERPRISE_CONTRACT_STATUS } from '../../types';
-import { EnterpriseContractRow } from '../EnterpriseContractRow';
+import { WrappedEnterpriseContractRow } from '../EnterpriseContractRow';
 
 jest.mock('react-router-dom', () => {
   const actual = jest.requireActual('react-router-dom');
@@ -15,20 +16,38 @@ jest.mock('react-router-dom', () => {
     useParams: jest.fn(() => ({ appName: 'application' })),
   };
 });
+const mockSetExpandedRowIndex = jest.fn();
 
+const dummySuccessRowData = {
+  title: 'dummyTitle',
+  status: ENTERPRISE_CONTRACT_STATUS.successes,
+  component: 'component-1',
+  description: 'dummy description',
+} as UIEnterpriseContractData;
+
+const dumpFailRowData = {
+  title: 'dummyTitle',
+  status: ENTERPRISE_CONTRACT_STATUS.violations,
+  component: 'component-1',
+  description: 'dummy description',
+  msg: 'Fail',
+  timestamp: '2022-01-01T00:00:00Z',
+  collection: ['abcd', 'efg'],
+};
+
+const cusmtomDummyData = {
+  sortedECResult: [dummySuccessRowData, dumpFailRowData],
+};
 describe('EnterpriseContractRow', () => {
   mockUseNamespaceHook('test-ns');
 
   it('should render the component', () => {
     render(
-      <EnterpriseContractRow
-        rowIndex={1}
-        data={{
-          title: 'dummyTitle',
-          status: ENTERPRISE_CONTRACT_STATUS.successes,
-          component: 'component-1',
-          description: 'dummy description',
-        }}
+      <WrappedEnterpriseContractRow
+        customData={cusmtomDummyData}
+        obj={dummySuccessRowData}
+        setExpandedRowIndex={mockSetExpandedRowIndex}
+        expandedRowIndex={1}
       />,
     );
     screen.getByText('dummyTitle');
@@ -39,23 +58,17 @@ describe('EnterpriseContractRow', () => {
 
   it('should render Failed rule and failure message in table', () => {
     render(
-      <EnterpriseContractRow
-        rowIndex={1}
-        data={{
-          title: 'dummyTitle',
-          status: ENTERPRISE_CONTRACT_STATUS.violations,
-          component: 'component-1',
-          description: 'dummy description',
-          msg: 'Fail',
-          timestamp: '2022-01-01T00:00:00Z',
-          collection: ['abcd', 'efg'],
-        }}
+      <WrappedEnterpriseContractRow
+        customData={cusmtomDummyData}
+        obj={dumpFailRowData}
+        setExpandedRowIndex={mockSetExpandedRowIndex}
+        expandedRowIndex={1}
       />,
     );
     screen.getByText('dummyTitle');
     screen.getByText('component-1');
     screen.getByText('Failed');
-    fireEvent.click(screen.getByTestId('ec-expand-row'));
+    fireEvent.click(screen.queryAllByTestId('ec-expand-row')[0]);
     screen.getByText('Effective from');
     screen.getByText('Collection');
     screen.getByText('abcd, efg');
