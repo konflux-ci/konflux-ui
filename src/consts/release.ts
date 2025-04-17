a2426e53baa539e106e2b6f699a3c55be45a5932
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-04-17 11:11:24 +0200

    feat(utils/release-actions): add releaseRerun function
    Introduce `releaseRerun` utility to create a new Release resource based on
    an existing one. This approach simulates a "rerun" by cloning the original
    release with a new name and updated labels.

---
+++ src/consts/release.ts
@@ -0,0 +1,5 @@
+export enum ReleaseLabel {
+  APPLICATION = 'appstudio.openshift.io/application',
+  AUTOMATED = 'release.appstudio.openshift.io/automated',
+  AUTHOR = 'release.appstudio.openshift.io/author',
+}
