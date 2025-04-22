103209875da9e7e6d7a7a08992d8d627f1eae096
Michal Marcin <marcin.michal02@gmail.com>
2025-04-22 14:23:55 +0200

    chore: update SecurityEnterpriseContractTab unit tests

--- src/components/EnterpriseContract/__tests__/SecurityEnterpriseContractTab.spec.tsx
+++ src/components/EnterpriseContract/__tests__/SecurityEnterpriseContractTab.spec.tsx
@@ -1,15 +1,29 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+import { FilterContextProvider } from '~/components/Filter/generic/FilterContext';
+import { mockUseSearchParamBatch } from '~/unit-test-utils/mock-useSearchParam';
 import { routerRenderer } from '../../../utils/test-utils';
 import { mockEnterpriseContractUIData } from '../__data__/mockEnterpriseContractLogsJson';
 import { SecurityEnterpriseContractTab } from '../SecurityEnterpriseContractTab';
 import { useEnterpriseContractResults } from '../useEnterpriseContractResultFromLogs';
 import '@testing-library/jest-dom';
 
+jest.useFakeTimers();
+
 jest.mock('../useEnterpriseContractResultFromLogs', () => ({
   useEnterpriseContractResults: jest.fn(),
 }));
 
+jest.mock('~/hooks/useSearchParam', () => ({
+  useSearchParamBatch: () => mockUseSearchParamBatch(),
+}));
+
 const mockUseEnterpriseContractResults = useEnterpriseContractResults as jest.Mock;
+
+const securityEnterpriseContracts = (pipelineRun: string) => (
+  <FilterContextProvider filterParams={['rule', 'status', 'component']}>
+    <SecurityEnterpriseContractTab pipelineRun={pipelineRun} />
+  </FilterContextProvider>
+);
 
 describe('SecurityEnterpriseContractTab', () => {
   beforeEach(() => {
@@ -21,53 +35,81 @@ describe('SecurityEnterpriseContractTab', () => {
   it('should render empty state for security tab when pods are missing', () => {
     mockUseEnterpriseContractResults.mockReturnValue([undefined, true]);
 
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy" />);
+    routerRenderer(securityEnterpriseContracts('dummy'));
     screen.getByTestId('security-tab-empty-state');
   });
 
   it('should render component security tab', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy" />);
+    routerRenderer(securityEnterpriseContracts('dummy'));
     screen.getByText('Missing CVE scan results');
   });
 
   it('should filter out results based on the name input field', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy" />);
+    const view = routerRenderer(securityEnterpriseContracts('dummy'));
     screen.getByText('Missing CVE scan results');
     fireEvent.input(screen.getByPlaceholderText('Filter by rule...'), {
       target: { value: 'No tasks' },
     });
+
+    act(() => {
+      jest.advanceTimersByTime(700);
+    });
+    view.rerender(securityEnterpriseContracts('dummy'));
     expect(screen.queryByText('Missing CVE scan results')).not.toBeInTheDocument();
+
     fireEvent.input(screen.getByPlaceholderText('Filter by rule...'), { target: { value: '' } });
+    act(() => {
+      jest.advanceTimersByTime(700);
+    });
+    view.rerender(securityEnterpriseContracts('dummy'));
     screen.getByText('Missing CVE scan results');
   });
 
-  it('should filter out based on the status dropdown', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy-1" />);
+  it('should filter out based on the status dropdown', async () => {
+    const view = routerRenderer(securityEnterpriseContracts('dummy-1'));
     screen.getByText('Missing CVE scan results');
-    fireEvent.click(screen.getByRole('button', { name: 'Status filter menu' }));
-    fireEvent.click(screen.getByLabelText('Success'));
-    expect(screen.queryByText('Missing CVE scan results')).not.toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: /status filter menu/i }));
+    fireEvent.click(
+      screen.getByLabelText(/success/i, {
+        selector: 'input',
+      }),
+    );
+    view.rerender(securityEnterpriseContracts('dummy-1'));
+    await waitFor(() => {
+      expect(screen.queryByText('Missing CVE scan results')).not.toBeInTheDocument();
+    });
     fireEvent.click(screen.getAllByText('Clear all filters')[0]);
+    view.rerender(securityEnterpriseContracts('dummy-1'));
     screen.getByText('Missing CVE scan results');
   });
 
   it('should show empty state when no search result found', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy-1" />);
+    const view = routerRenderer(securityEnterpriseContracts('dummy-1'));
     screen.getByText('Missing CVE scan results');
-    fireEvent.click(screen.getByRole('button', { name: 'Status filter menu' }));
-    fireEvent.click(screen.getByLabelText('Failed'));
+    fireEvent.click(screen.getByRole('button', { name: /status filter menu/i }));
+    fireEvent.click(
+      screen.getByLabelText(/failed/i, {
+        selector: 'input',
+      }),
+    );
+    view.rerender(securityEnterpriseContracts('dummy-1'));
     screen.getByText('Missing CVE scan results');
     fireEvent.input(screen.getByPlaceholderText('Filter by rule...'), {
       target: { value: 'No tasks' },
     });
+    act(() => {
+      jest.advanceTimersByTime(700);
+    });
+    view.rerender(securityEnterpriseContracts('dummy-1'));
     expect(screen.queryByText('Missing CVE scan results')).not.toBeInTheDocument();
     screen.getByText('No results found');
     fireEvent.click(screen.getAllByText('Clear all filters')[1]);
+    view.rerender(securityEnterpriseContracts('dummy-1'));
     screen.getByText('Missing CVE scan results');
   });
 
   it('should sort by Status', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy-1" />);
+    routerRenderer(securityEnterpriseContracts('dummy-1'));
     const status = screen.getAllByTestId('rule-status');
     expect(status[0].textContent.trim()).toEqual('Failed');
     fireEvent.click(screen.getAllByText('Status')[1]);
@@ -76,7 +118,7 @@ describe('SecurityEnterpriseContractTab', () => {
   });
 
   it('should render result summary', () => {
-    routerRenderer(<SecurityEnterpriseContractTab pipelineRun="dummy-1" />);
+    routerRenderer(securityEnterpriseContracts('dummy-1'));
     const resultSummary = screen.getByTestId('result-summary');
     const status = resultSummary.getElementsByTagName('span');
     expect(status[0].textContent.trim()).toBe('Failed');
