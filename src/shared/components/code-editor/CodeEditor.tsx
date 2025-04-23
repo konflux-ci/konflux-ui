ea12c2958a8c7d4923e26150891b63b54f702d1a
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-04-23 14:14:50 +0200

    feat(shared/components/code-editor): add support for YAML file display
    Introduce new components based on PatternFly's CodeEditor to render
    code files. Currently, only ".yaml" files are supported via the
    YAMLCodeEditor component. Includes reusable hooks, types, and shortcuts
    to support editor functionality.

---
+++ src/shared/components/code-editor/CodeEditor.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { loader } from '@monaco-editor/react';
+import {
+  CodeEditor as PatternFlyCodeEditor,
+  Language,
+  EditorDidMount,
+  CodeEditorProps,
+} from '@patternfly/react-code-editor';
+import * as monacoConfig from 'monaco-editor/esm/vs/editor/editor.api';
+import { useShortcutPopover } from './hooks/useShortcutPopover';
+
+loader.config({ monaco: monacoConfig });
+
+export type Props = {
+  code: string;
+  language?: Language;
+  height?: CodeEditorProps['height'];
+  showShortcuts?: boolean;
+  onEditorDidMount: EditorDidMount;
+};
+
+export const CodeEditor: React.FC<Props> = ({
+  code,
+  height = '500px',
+  showShortcuts = true,
+  language = Language.yaml,
+  onEditorDidMount,
+}) => {
+  const shortcutPopover = useShortcutPopover();
+
+  return (
+    <div style={{ flexGrow: 1 }}>
+      <PatternFlyCodeEditor
+        data-test="monaco-editor"
+        language={language}
+        code={code}
+        options={{
+          readOnly: true,
+        }}
+        onEditorDidMount={onEditorDidMount}
+        isDarkTheme
+        height={height}
+        shortcutsPopoverProps={showShortcuts ? shortcutPopover : undefined}
+      />
+    </div>
+  );
+};
