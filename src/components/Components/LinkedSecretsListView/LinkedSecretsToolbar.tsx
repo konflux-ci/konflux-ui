a9d42a90b360a52e21091469f55f124ff900da67
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-04-29 20:11:41 +0200

    feat(LinkedSecretsListView/LinkedSecretsToolbar): add component
    Add "LinkedSecretsToolbar" component, which contains a search input to allow
    the user to filter the list of linked secrets by name.

---
+++ src/components/Components/LinkedSecretsListView/LinkedSecretsToolbar.tsx
@@ -0,0 +1,28 @@
+import { SearchInput, Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
+import { useSearchParam } from '../../../hooks/useSearchParam';
+
+export const LinkedSecretsToolbar: React.FC = () => {
+  const [nameFilter, setNameFilter] = useSearchParam('name', '');
+
+  return (
+    <Toolbar
+      data-test="linked-secrets-list-toolbar"
+      clearFiltersButtonText="Clear filters"
+      clearAllFilters={() => setNameFilter('')}
+    >
+      <ToolbarContent>
+        <ToolbarItem>
+          <SearchInput
+            name="nameInput"
+            data-test="name-input-filter"
+            type="search"
+            aria-label="name filter"
+            placeholder="Filter by name..."
+            onChange={(_, name) => setNameFilter(name)}
+            value={nameFilter}
+          />
+        </ToolbarItem>
+      </ToolbarContent>
+    </Toolbar>
+  );
+};
