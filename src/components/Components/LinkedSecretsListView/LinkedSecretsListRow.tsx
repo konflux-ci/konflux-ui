538ba0041a041bceb8b445b3c06965669a01ebed
janaki29 <dehankarjanaki@gmail.com>
2025-05-27 21:07:47 +0530

    chore(unlink-secrets): code restruture

--- src/components/Components/LinkedSecretsListView/LinkedSecretsListRow.tsx
+++ src/components/Components/LinkedSecretsListView/LinkedSecretsListRow.tsx
@@ -30,11 +30,7 @@ export const LinkedSecretsListRow: React.FC<RowFunctionArgs<SecretKind>> = ({ ob
       </TableData>
 
       <TableData className={linkedSecretsTableColumnClasses.actions}>
-        {/* TODO: onClick logic will be implemented in another ticket*/}
-        {/* eslint-disable-next-line no-alert */}
-        {/* <Button variant="secondary" onClick={() => alert('TODO')}> */}
         <UnlinkSecretView secret={secret} />
-        {/* </Button> */}
       </TableData>
 
       <TableData className={linkedSecretsTableColumnClasses.kebab}>
