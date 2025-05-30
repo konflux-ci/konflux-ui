dc6a4f3c21e561e66fbaa3d64a2c8eb7730aa8dc
Michal Marcin <marcin.michal02@gmail.com>
2025-05-30 15:32:21 +0200

    fix(KFLUXUI-432): hide additional secret labels into expandable section

--- src/components/Secrets/SecretsListView/SecretsList.tsx
+++ src/components/Secrets/SecretsListView/SecretsList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useIsOnFeatureFlag } from '~/feature-flags/hooks';
-import { Table } from '../../../shared';
+import { RowFunctionArgs, Table } from '../../../shared';
 import { SecretKind } from '../../../types';
 import SecretsListHeader from './SecretsListHeader';
 import SecretsListHeaderWithComponents from './SecretsListHeaderWithComponents';
@@ -17,6 +17,19 @@ const SecretsList: React.FC<React.PropsWithChildren<SecretsListProps>> = ({ secr
     ? SecretsListHeaderWithComponents
     : SecretsListHeader;
   const Row = isBuildServiceAccountFeatureOn ? SecretsListRowWithComponents : SecretsListRow;
+  const [expandedRowIndexes, setExpandedRowIndexes] = React.useState<Set<number>>(new Set());
+
+  const toggleRow = (index: number) => {
+    setExpandedRowIndexes((prev) => {
+      const next = new Set(prev);
+      if (next.has(index)) {
+        next.delete(index);
+      } else {
+        next.add(index);
+      }
+      return next;
+    });
+  };
 
   return (
     <>
@@ -24,7 +37,13 @@ const SecretsList: React.FC<React.PropsWithChildren<SecretsListProps>> = ({ secr
         data={secrets}
         aria-label="Secret List"
         Header={Header}
-        Row={Row}
+        Row={(props: RowFunctionArgs<SecretKind>) => {
+          const rowIndex = secrets.findIndex((r) => r === props.obj);
+          const isExpanded = expandedRowIndexes.has(rowIndex);
+
+          return Row({ obj: props.obj, isExpanded, onToggleExpand: () => toggleRow(rowIndex) });
+        }}
+        expandedRowIndexes={expandedRowIndexes}
         loaded
         getRowProps={(obj: SecretKind) => ({
           id: obj.metadata.name,
