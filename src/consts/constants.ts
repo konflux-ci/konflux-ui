15ebfd7db94b4c9999867f881304e350cdfe7e58
Michal Marcin <marcin.michal02@gmail.com>
2025-06-03 22:51:50 +0200

    fix(KONFLUX-8558): don't redirect public instance users to internal docs
    Assisted by: Cursor

--- src/consts/constants.ts
+++ src/consts/constants.ts
@@ -3,3 +3,5 @@ export const REPO_PUSH = 'repo_push';
 export const PLACEHOLDER = '<PLACEHOLDER>';
 export const NO_RESULTS = 'no results';
 export const TYPEAHEAD_THRESHOLD = 10;
+export const INTERNAL_INSTANCE_REGEX =
+  /^(stone-(prod|stage)|kflux-ocp)-p\d{2}\.[a-z0-9]{4}\.p1\.openshiftapps\.com$/;
