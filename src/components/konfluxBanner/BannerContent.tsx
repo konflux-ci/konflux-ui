7004804d737ea9d6c3f952b3bf25734a78d93ce6
Cara Wang <wlin@redhat.com>
2025-07-09 14:09:23 +0800

    feat(KFLUXUI-507): remove the repeatType field from banner

--- src/components/konfluxBanner/BannerContent.tsx
+++ src/components/konfluxBanner/BannerContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Flex } from '@patternfly/react-core';
 import { BellIcon } from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 import { ExclamationTriangleIcon } from '@patternfly/react-icons/dist/esm/icons/exclamation-triangle-icon';
 import { InfoCircleIcon } from '@patternfly/react-icons/dist/esm/icons/info-circle-icon';
@@ -25,8 +26,14 @@ const typeToIcon = (type: BannerType) => {
 };
 
 export const BannerContent: React.FC<BannerContentProps> = React.memo(({ type, summary }) => (
-  <div style={{ display: 'flex', alignItems: 'center' }}>
+  <Flex
+    alignItems={{ default: 'alignItemsCenter' }}
+    justifyContent={{ default: 'justifyContentCenter' }}
+    flexWrap={{ default: 'nowrap' }}
+    spaceItems={{ default: 'spaceItemsSm' }}
+    style={{ width: '100%' }}
+  >
     {typeToIcon(type)}
     <SyncMarkdownView content={summary} inline />
-  </div>
+  </Flex>
 ));
