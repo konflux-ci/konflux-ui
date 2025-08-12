457c119227963eab5665f4db0082bdde4e3d82bc
Brian Cook <bcook@redhat.com>
2025-08-12 15:42:01 -0400

    refactor: improve modal launcher typing and absolute imports

--- src/components/NamespaceList/ManageVisibilityModalLauncher.tsx
+++ src/components/NamespaceList/ManageVisibilityModalLauncher.tsx
@@ -1,11 +1,12 @@
+import { ModalVariant } from '@patternfly/react-core';
+import { createModalLauncher } from '~/components/modal/createModalLauncher';
+import ManageVisibilityModal from '~/components/NamespaceList/ManageVisibilityModal';
 import { NamespaceKind } from '~/types';
-import { createModalLauncher } from '../modal/createModalLauncher';
-import ManageVisibilityModal from './ManageVisibilityModal';
 
 export const createManageVisibilityModalLauncher = (namespace: NamespaceKind) =>
   createModalLauncher(ManageVisibilityModal, {
     'data-test': 'manage-visibility-modal',
-    variant: 'small',
+    variant: ModalVariant.small,
     title: 'Manage visibility',
     description:
       'Manage visibility for a namespace. Private namespaces are only accessible to members, while public namespaces allow read-only access to all authenticated users.',
