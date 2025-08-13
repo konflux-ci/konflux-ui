bd365e8066c6c830021e5a298a8cf9e5c5eaecdc
Paul Cho <pacho@redhat.com>
2025-08-13 18:58:01 +0000

    fix: sanitize status_page_url

--- src/components/Overview/__tests__/AboutSection.spec.tsx
+++ src/components/Overview/__tests__/AboutSection.spec.tsx
@@ -43,11 +43,16 @@ describe('About section with status page card', () => {
   it('should render the links correctly in about section', () => {
     render(<AboutSection />);
     expect(screen.getByRole('link', { name: 'repository' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'View Status Page' })).toBeInTheDocument();
+    const statusLink = screen.getByRole('link', { name: 'View Status Page' });
+    expect(statusLink).toBeInTheDocument();
+    expect(statusLink).toHaveAttribute('href', 'https://status-page.local/');
+    expect(statusLink).toHaveAttribute('target', '_blank');
+    expect(statusLink).toHaveAttribute('rel', expect.stringContaining('noopener'));
+    expect(statusLink).toHaveAttribute('rel', expect.stringContaining('noreferrer'));
   });
 });
 
-describe('About section without status page card', () => {
+describe('About section without status page card due to missing url', () => {
   beforeEach(() => {
     mockUseQuery
       .mockReturnValueOnce({
@@ -59,6 +64,38 @@ describe('About section without status page card', () => {
         isLoading: true,
       });
     mockUseKonfluxPublicInfo.mockReturnValue([{}]);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render everything correctly in about section', () => {
+    render(<AboutSection />);
+    screen.getByText('About');
+    screen.getByText('Contact us');
+    expect(screen.queryByText('Status Page')).not.toBeInTheDocument();
+  });
+
+  it('should render the links correctly in about section', () => {
+    render(<AboutSection />);
+    expect(screen.getByRole('link', { name: 'repository' })).toBeInTheDocument();
+    expect(screen.queryByRole('link', { name: 'View Status Page' })).not.toBeInTheDocument();
+  });
+});
+
+describe('About section without status page card due to bad url', () => {
+  beforeEach(() => {
+    mockUseQuery
+      .mockReturnValueOnce({
+        data: undefined,
+        isLoading: true,
+      })
+      .mockReturnValueOnce({
+        data: undefined,
+        isLoading: true,
+      });
+    // eslint-disable-next-line camelcase
+    mockUseKonfluxPublicInfo.mockReturnValue([{ status_page_url: 'badurl' }]);
   });
   afterEach(() => {
     jest.clearAllMocks();
