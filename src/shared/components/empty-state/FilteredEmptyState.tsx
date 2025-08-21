4a14459943ffa822d8b6f663cbc1ba869c94b26d
milantaky <93554604+milantaky@users.noreply.github.com>
2025-08-21 16:04:19 +0200

    feat(KFLUXUI-607): add icons for dark theme (#352)
    * fix: changed intro banner svgs to white in dark mode
    
    * fix: changed Commit Icons to white when in dark mode
    
    * fix: changes assets color values, modifies rendering SVGs
    
    * fix: svg tests pass
    
    * Merge branch 'KFLUXUI-607/add-icons-for-dark-theme' of github.com:milantaky/konflux-ui into KFLUXUI-607/add-icons-for-dark-theme
    
    * fix: accepts coderabbit suggestions
    
    * Merge branch 'KFLUXUI-607/add-icons-for-dark-theme' of github.com:milantaky/konflux-ui into KFLUXUI-607/add-icons-for-dark-theme
    
    * fix: updates svg preview, chages svg import, improves commit and PR icons svgs
    
    * fix: codeRabbit issue with Relation icon
    
    * fix: 404 icon fixed, getting started card in components, webpack config
    
    * fix: commented lines cleanup
    
    ---------
    
    Co-authored-by: Sahil Budhwar <sahilbudhwar143@gmail.com>

--- src/shared/components/empty-state/FilteredEmptyState.tsx
+++ src/shared/components/empty-state/FilteredEmptyState.tsx
@@ -10,13 +10,11 @@ import {
   EmptyStateHeader,
   EmptyStateFooter,
 } from '@patternfly/react-core';
-import emptySearchImgUrl from '../../../assets/Not-found.svg';
+import EmptySearchImgUrl from '../../assets/Not-found.svg';
 
 import './EmptyState.scss';
 
-const EmptyStateImg = () => (
-  <img className="app-empty-state__icon" src={emptySearchImgUrl} alt="" />
-);
+const EmptyStateImg = () => <EmptySearchImgUrl className="app-empty-state__icon" role="img" />;
 
 const FilteredEmptyState: React.FC<
   React.PropsWithChildren<
