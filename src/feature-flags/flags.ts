1d953dec7318658cb73ddddb7143665476fc5ac9
Joao Pedro Poloni Ponce <jpolonip@redhat.com>
2025-09-10 16:16:24 -0300

    refactor: use kflux-668 as base for the hook
    Rebase branch with the chnages introduced in KFLUXUI-668 and adjust
    functions to use usePipelineRunsV2

--- src/feature-flags/flags.ts
+++ src/feature-flags/flags.ts
@@ -61,6 +61,17 @@ const InternalFLAGS = {
     defaultEnabled: false,
     status: 'wip',
   },
+  'kubearchive-logs': {
+    key: 'kubearchive-logs',
+    description: 'Use Kubearchive to fetch logs instead of Tekton',
+    defaultEnabled: false,
+    status: 'wip',
+    guard: {
+      allOf: ['isKubearchiveEnabled'],
+      failureReason: 'Kubearchive is not installed on this cluster',
+      visibleInFeatureFlagPanel: true,
+    },
+  },
   'pipelineruns-kubearchive': {
     key: 'pipelineruns-kubearchive',
     description: 'Use KubeArchive as data source for PipelineRuns instead of Tekton Results',
