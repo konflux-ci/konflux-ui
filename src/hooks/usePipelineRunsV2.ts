03f78b99e8c9e34529c1dc9c481b5628b6c49b61
Joao Pedro Poloni Ponce <jpolonip@redhat.com>
2025-09-15 19:54:34 -0300

    feat: add unit tests

--- src/hooks/usePipelineRunsV2.ts
+++ src/hooks/usePipelineRunsV2.ts
@@ -344,7 +344,7 @@ export const usePipelineRunsForCommitV2 = (
           matchExpressions:
             filterByComponents && componentNames.length > 0
               ? [{ key: PipelineRunLabel.COMPONENT, operator: 'In', values: componentNames }]
-              : undefined,
+              : [],
           filterByCommit: commit,
         },
         enabled: isKubearchiveEnabled,
@@ -363,10 +363,10 @@ export const usePipelineRunsForCommitV2 = (
   );
 
   return React.useMemo(() => {
-    if (plrsLoaded) {
-      return [pipelineRuns as PipelineRunKind[], plrsLoaded, plrError, getNextPage, nextPageProps];
+    if (!plrsLoaded || plrError) {
+      return [[], plrsLoaded, plrError ?? 'Error', undefined, undefined];
     }
-    return [[], plrsLoaded, plrError ?? 'Error', undefined, undefined];
+    return [pipelineRuns as PipelineRunKind[], plrsLoaded, plrError, getNextPage, nextPageProps];
   }, [
     // usePipelineRunV2 variables
     pipelineRuns,
