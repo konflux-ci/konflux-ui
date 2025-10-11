5a2cc31e6b821939f175280aa0e10f9efbff4ba3
Rony Gong <qgong@redhat.com>
2025-10-11 14:42:12 +0800

    feat: provide the option of listing single latest release

--- src/components/ReleaseMonitor/__data__/mock-release-data.ts
+++ src/components/ReleaseMonitor/__data__/mock-release-data.ts
@@ -9,7 +9,7 @@ export const mockReleases: ReleaseKind[] = [
       name: 'test-release-1',
       creationTimestamp: '2023-02-01T10:30:00Z',
       labels: {
-        'appstudio.openshift.io/application': 'foo',
+        'appstudio.openshift.io/application': 'test',
         'appstudio.openshift.io/component': 'foo-component',
       },
       namespace: 'namespace-1',
@@ -80,6 +80,36 @@ export const mockReleases: ReleaseKind[] = [
     status: {
       startTime: '2023-01-01T10:30:00Z',
       completionTime: '2023-01-01T10:31:10Z',
+      target: 'test-target',
+      conditions: [
+        {
+          message: 'Release processing failed on managed pipelineRun',
+          reason: 'Failed',
+          status: 'False',
+          type: ReleaseCondition.Released,
+        },
+      ],
+    },
+  },
+  {
+    apiVersion: 'appstudio.redhat.com/v1alpha1',
+    kind: 'Release',
+    metadata: {
+      name: 'test-release-4',
+      creationTimestamp: '2023-01-01T10:30:00Z',
+      labels: {
+        'appstudio.openshift.io/application': 'foo',
+        'appstudio.openshift.io/component': 'bar-01-component',
+      },
+      namespace: 'namespace-2',
+    },
+    spec: {
+      releasePlan: 'test-plan-3',
+      snapshot: 'test-snapshot-3',
+    },
+    status: {
+      startTime: '2023-01-01T10:30:00Z',
+      completionTime: '2023-01-02T10:31:10Z',
       target: 'test-target',
       conditions: [
         {
