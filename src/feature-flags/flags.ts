36a77a689d28f59b0486f9588c1828d73ddb83bc
Sahil Budhwar <sahilbudhwar143@gmail.com>
2025-11-17 16:23:26 +0530

    chore: enable kubearchive flags by default

--- src/feature-flags/flags.ts
+++ src/feature-flags/flags.ts
@@ -64,7 +64,7 @@ const InternalFLAGS = {
   'kubearchive-logs': {
     key: 'kubearchive-logs',
     description: 'Use Kubearchive to fetch logs instead of Tekton',
-    defaultEnabled: false,
+    defaultEnabled: true,
     status: 'wip',
     guard: {
       allOf: ['isKubearchiveEnabled'],
@@ -86,7 +86,7 @@ const InternalFLAGS = {
   'taskruns-kubearchive': {
     key: 'taskruns-kubearchive',
     description: 'Use Kubearchive as data source for TaskRuns instead of Tekton results',
-    defaultEnabled: false,
+    defaultEnabled: true,
     status: 'wip',
     guard: {
       allOf: ['isKubearchiveEnabled'],
@@ -97,7 +97,7 @@ const InternalFLAGS = {
   'pipelineruns-kubearchive': {
     key: 'pipelineruns-kubearchive',
     description: 'Use KubeArchive as data source for PipelineRuns instead of Tekton Results',
-    defaultEnabled: false,
+    defaultEnabled: true,
     status: 'wip',
     guard: {
       allOf: ['isKubearchiveEnabled'],
