159e0d9714923069dc82574ae69937b97209b00a
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-12-05 11:14:22 +0100

    refactor(RunResultsList): use NameValueList
    Update to use NameValueList component.

--- src/components/PipelineRun/PipelineRunDetailsView/tabs/RunResultsList.tsx
+++ src/components/PipelineRun/PipelineRunDetailsView/tabs/RunResultsList.tsx
@@ -1,20 +1,6 @@
 import * as React from 'react';
-import {
-  Bullseye,
-  DescriptionList,
-  DescriptionListDescription,
-  DescriptionListGroup,
-  DescriptionListTerm,
-  EmptyState,
-  EmptyStateBody,
-  EmptyStateVariant,
-} from '@patternfly/react-core';
-import { css } from '@patternfly/react-styles';
-import { Table /* data-codemods */, Thead, Tbody, Th, Td, Tr } from '@patternfly/react-table';
 import { TektonResultsRun } from '../../../../types';
-import { handleURLs } from '../../../../utils/render-utils';
-
-import './RunResultsList.scss';
+import NameValueList from './NameValueList';
 
 type Props = {
   results: TektonResultsRun[];
@@ -27,43 +13,13 @@ const RunResultsList: React.FC<React.PropsWithChildren<Props>> = ({
   status,
   compressed,
 }) => (
-  <DescriptionList
-    className={css('run-results-list', compressed && 'm-compressed')}
-    data-test="pipeline-run-details"
-    columnModifier={{
-      default: '1Col',
-    }}
-  >
-    <DescriptionListGroup>
-      <DescriptionListTerm>Results</DescriptionListTerm>
-      <DescriptionListDescription>
-        {status !== 'Failed' ? (
-          <Table aria-label="results">
-            <Thead>
-              <Tr>
-                <Th>Name</Th>
-                <Th>Value</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {results.map(({ name, value }) => (
-                <Tr key={`row-${name}`}>
-                  <Td className="run-results-list__key">{name}</Td>
-                  <Td className="run-results-list__value">{handleURLs(value)}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        ) : (
-          <Bullseye>
-            <EmptyState variant={EmptyStateVariant.full}>
-              <EmptyStateBody>No results available due to failure</EmptyStateBody>
-            </EmptyState>
-          </Bullseye>
-        )}
-      </DescriptionListDescription>
-    </DescriptionListGroup>
-  </DescriptionList>
+  <NameValueList
+    items={results}
+    descriptionListTestId="pipeline-run-details"
+    title="Results"
+    status={status}
+    compressed={compressed}
+  />
 );
 
 export default RunResultsList;
