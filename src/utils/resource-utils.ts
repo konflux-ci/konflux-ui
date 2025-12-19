2dd060b62e64cfc4094390e244d705e287d14d34
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-12-19 15:04:17 +0100

    refactor(resource-utils): extract function to filter out stale running PipelineRuns
    Extract from tekton-results.ts a function to filter out stale running
    PipelineRuns.

--- src/utils/resource-utils.ts
+++ src/utils/resource-utils.ts
@@ -1,5 +1,31 @@
+import { PipelineRunKind, PipelineRunKindV1Beta1 } from '../types';
 import { K8sResourceCommon } from '../types/k8s';
 
 export const filterDeletedResources = <R extends K8sResourceCommon[]>(resources: R) => {
   return resources.filter((res) => !res.metadata.deletionTimestamp);
+};
+
+/*
+  When pipelineruns are running, the etcd would keep their results.
+  While the tekton record would keep the conditions as:
+  "conditions": [
+    {
+      "type": "Succeeded",
+      "reason": "Running",
+      "status": "Unknown",
+    ...
+  Deleting pipelines from etcd makes the tekton record would be never updated as others.
+  So for those tekton results, it is useless to users and we need to filter them out.
+  Otherwise, these jobs would be always shown as 'Running' and bring unexpected troubles.
+  */
+export const filterOutStaleRunningPipelineRunsFromArchive = (
+  pipelineRuns: (PipelineRunKindV1Beta1 | PipelineRunKind)[] | undefined,
+): (PipelineRunKindV1Beta1 | PipelineRunKind)[] | undefined => {
+  return pipelineRuns?.filter((pipelinerun) => {
+    return (
+      pipelinerun?.status?.conditions?.every(
+        (c) => !(c.status === 'Unknown' && c.type === 'Succeeded' && c.reason === 'Running'),
+      ) ?? true
+    );
+  });
 };
