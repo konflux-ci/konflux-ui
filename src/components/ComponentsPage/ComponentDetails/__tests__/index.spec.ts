3c77060c798eadb75def27702042cf5a524de923
Rodrigo R. Galvão <rgalvao@redhat.com>
2026-01-29 13:51:12 +0100

    fixup! feat(ComponentsPage): add component details view

---
+++ src/components/ComponentsPage/ComponentDetails/__tests__/index.spec.ts
@@ -0,0 +1,55 @@
+import { k8sQueryGetResource } from '../../../../k8s';
+import { ComponentModel } from '../../../../models';
+import { RouterParams } from '../../../../routes/utils';
+import {
+  componentDetailsViewLoader,
+  ComponentDetailsTab,
+  ComponentDetailsViewLayout,
+} from '../index';
+
+jest.mock('../../../../k8s', () => ({
+  k8sQueryGetResource: jest.fn(),
+}));
+
+jest.mock('../../../../utils/rbac', () => ({
+  createLoaderWithAccessCheck: jest.fn((loader) => loader),
+}));
+
+jest.mock('../ComponentDetailsView', () => ({
+  __esModule: true,
+  default: () => null,
+}));
+
+jest.mock('../../../Components/ComponentDetails/tabs/ComponentDetailsTab', () => ({
+  __esModule: true,
+  default: () => null,
+}));
+
+const k8sQueryGetResourceMock = k8sQueryGetResource as jest.Mock;
+
+describe('ComponentDetails index', () => {
+  it('should export layout and tab components', () => {
+    expect(ComponentDetailsViewLayout).toBeDefined();
+    expect(ComponentDetailsTab).toBeDefined();
+  });
+
+  it('should call k8sQueryGetResource in loader', async () => {
+    k8sQueryGetResourceMock.mockResolvedValue({ metadata: { name: 'my-component' } });
+
+    await componentDetailsViewLoader({
+      params: {
+        [RouterParams.workspaceName]: 'test-ns',
+        [RouterParams.componentName]: 'my-component',
+      },
+      request: undefined,
+    });
+
+    expect(k8sQueryGetResourceMock).toHaveBeenCalledWith({
+      model: ComponentModel,
+      queryOptions: {
+        ns: 'test-ns',
+        name: 'my-component',
+      },
+    });
+  });
+});
