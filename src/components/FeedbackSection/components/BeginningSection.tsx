40ae405f96499da9da1258691fcd36a82875232d
Abhi <abhinandan13jan@gmail.com>
2026-02-14 01:56:35 +0530

    feat(feedback-modal): tweak css to make it similar to pf-user-feedback

--- src/components/FeedbackSection/components/BeginningSection.tsx
+++ src/components/FeedbackSection/components/BeginningSection.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import {
-  Stack,
-  StackItem,
   Text,
   TextContent,
   TextVariants,
   Card,
   CardTitle,
   CardBody,
+  PanelHeader,
+  PanelMain,
+  PanelMainBody,
+  FlexItem,
 } from '@patternfly/react-core';
 import { FeedbackSections } from '../consts';
 
@@ -17,76 +19,84 @@ interface BeginningSectionProps {
 
 const BeginningSection: React.FC<BeginningSectionProps> = ({ setCurrentSection }) => {
   return (
-    <Stack hasGutter>
-      <StackItem>
-        <TextContent>
-          <Text component={TextVariants.h1}>Tell us how we&apos;re doing?</Text>
-        </TextContent>
-      </StackItem>
-      <StackItem>
-        <TextContent>
-          <Text component={TextVariants.small}>Help us make Konflux the bees knees</Text>
-        </TextContent>
-      </StackItem>
-      <StackItem className="feedback-modal__card-container">
-        <Card
-          className="feedback-modal__card-container-card"
-          onClick={() => setCurrentSection(FeedbackSections.FeedbackSection)}
-        >
-          <CardTitle>
-            <TextContent>
-              <Text component={TextVariants.p}>Share feedback</Text>
-            </TextContent>
-          </CardTitle>
-          <CardBody>
-            <TextContent>
-              <Text component={TextVariants.small}>
-                Please share your experience using Konflux to the product team
-              </Text>
-            </TextContent>
-          </CardBody>
-        </Card>
-      </StackItem>
-      <StackItem>
-        <Card
-          className="feedback-modal__card-container-card"
-          onClick={() => setCurrentSection(FeedbackSections.BugSection)}
-        >
-          <CardTitle>
-            <TextContent>
-              <Text component={TextVariants.p}>Report a bug</Text>
-            </TextContent>
-          </CardTitle>
-          <CardBody>
-            <TextContent>
-              <Text component={TextVariants.small}>
-                Describe the bug you encountered. For urgent issues, use #konflux-user-forum
-                instead.
-              </Text>
-            </TextContent>
-          </CardBody>
-        </Card>
-      </StackItem>
-      <StackItem>
-        <Card
-          className="feedback-modal__card-container-card"
-          onClick={() => setCurrentSection(FeedbackSections.FeatureSection)}
-        >
-          <CardTitle>
-            <TextContent>
-              <Text component={TextVariants.p}>Request a new feature</Text>
-            </TextContent>
-          </CardTitle>
-          <CardBody>
-            <TextContent>
-              <Text component={TextVariants.small}>
-                Share yout ideas for new feature to make Konflux even better for all users.
-              </Text>
-            </TextContent>
-          </CardBody>
-        </Card>
-      </StackItem>
-    </Stack>
+    <>
+      <PanelHeader className="feedback-modal__panel-header">
+        <FlexItem>
+          <TextContent>
+            <Text component={TextVariants.h1}>Tell us how we&apos;re doing?</Text>
+          </TextContent>
+        </FlexItem>
+        <FlexItem>
+          <TextContent>
+            <Text component={TextVariants.small}>Help us make Konflux the bees knees</Text>
+          </TextContent>
+        </FlexItem>
+      </PanelHeader>
+      <PanelMain className="feedback-modal__content-main">
+        <PanelMainBody className="feedback-modal__cards-list">
+          <Card
+            isCompact
+            isClickable
+            className="feedback-modal__section-card"
+            onClick={() => setCurrentSection(FeedbackSections.FeedbackSection)}
+          >
+            <CardTitle>
+              <TextContent>
+                <Text component={TextVariants.p}>Share feedback</Text>
+              </TextContent>
+            </CardTitle>
+            <CardBody>
+              <TextContent>
+                <Text component={TextVariants.small}>
+                  Please share your experience using Konflux to the product team
+                </Text>
+              </TextContent>
+            </CardBody>
+          </Card>
+
+          <Card
+            isCompact
+            isClickable
+            className="feedback-modal__section-card"
+            onClick={() => setCurrentSection(FeedbackSections.BugSection)}
+          >
+            <CardTitle>
+              <TextContent>
+                <Text component={TextVariants.p}>Report a bug</Text>
+              </TextContent>
+            </CardTitle>
+            <CardBody>
+              <TextContent>
+                <Text component={TextVariants.small}>
+                  Describe the bug you encountered. For urgent issues, use #konflux-user-forum
+                  instead.
+                </Text>
+              </TextContent>
+            </CardBody>
+          </Card>
+
+          <Card
+            isCompact
+            isClickable
+            className="feedback-modal__section-card"
+            onClick={() => setCurrentSection(FeedbackSections.FeatureSection)}
+          >
+            <CardTitle>
+              <TextContent>
+                <Text component={TextVariants.p}>Request a new feature</Text>
+              </TextContent>
+            </CardTitle>
+            <CardBody>
+              <TextContent>
+                <Text component={TextVariants.small}>
+                  Share yout ideas for new feature to make Konflux even better for all users.
+                </Text>
+              </TextContent>
+            </CardBody>
+          </Card>
+        </PanelMainBody>
+      </PanelMain>
+    </>
   );
 };
 
