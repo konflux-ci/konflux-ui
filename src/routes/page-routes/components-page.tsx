8412d4ddab5eae2f55149a1095e44730c9c03817
djanaki <djanaki@redhat.com>
2026-02-16 17:42:02 +0530

    chore(new-component-model): new component list page

--- src/routes/page-routes/components-page.tsx
+++ src/routes/page-routes/components-page.tsx
@@ -1,18 +1,30 @@
 import { RouteErrorBoundry } from '@routes/RouteErrorBoundary';
+import { IfFeature } from '~/feature-flags/hooks';
+import ComponentList from '../../components/ComponentList/ComponentList';
 import {
   ComponentDetailsTab,
   ComponentDetailsViewLayout,
   componentDetailsViewLoader,
 } from '../../components/ComponentsPage/ComponentDetails';
+import { FilterContextProvider } from '../../components/Filter/generic/FilterContext';
 import { COMPONENT_DETAILS_V2_PATH, COMPONENTS_PATH } from '../paths';
+
+const ComponentsListRoute: React.FC = () => {
+  return (
+    <FilterContextProvider filterParams={['name']}>
+      <ComponentList />
+    </FilterContextProvider>
+  );
+};
 
 const componentsPageRoutes = [
   {
     path: COMPONENTS_PATH.path,
-    lazy: async () => {
-      const { default: Component } = await import('~/components/ComponentsPage/ComponentsPage');
-      return { Component };
-    },
+    element: (
+      <IfFeature flag="components-page" fallback={null}>
+        <ComponentsListRoute />
+      </IfFeature>
+    ),
     errorElement: <RouteErrorBoundry />,
   },
   {
