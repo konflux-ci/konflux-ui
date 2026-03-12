3cc8e21c728e977fd035727980ecb37c8b23e6c1
milantaky <milan.taky@post.cz>
2026-03-12 18:50:13 +0100

    fix: returning cta function in a simpler way

--- src/components/DetailsPage/utils.ts
+++ src/components/DetailsPage/utils.ts
@@ -18,7 +18,5 @@ export const createDetailsPageAction = (action: DetailsPageActionInput): Details
   label: action.label,
   isDisabled: action.isDisabled ?? action.disabled,
   disabledTooltip: action.disabledTooltip,
-  onClick: () => {
-    if (typeof action.cta === 'function') void action.cta();
-  },
+  onClick: () => action.cta(),
 });
