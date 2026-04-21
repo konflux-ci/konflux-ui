1d7991b363ddb2b2f0332fe8e7e30e8976ceac49
milantaky <milan.taky@post.cz>
2026-04-21 10:44:22 +0200

    feat: widens select role select

--- src/components/UserAccess/UserAccessListView.tsx
+++ src/components/UserAccess/UserAccessListView.tsx
@@ -419,7 +419,7 @@ export const UserAccessListView: React.FC<React.PropsWithChildren<unknown>> = ()
 
           <FlexItem>
             <Flex direction={{ default: 'column' }}>
-              <FlexItem style={{ marginBottom: '0' }}>
+              <FlexItem style={{ marginBottom: 'var(--pf-v5-global--spacer--xs)' }}>
                 <Text data-test="user-access-change-role-label">New role*</Text>
               </FlexItem>
               <FlexItem>
@@ -432,6 +432,7 @@ export const UserAccessListView: React.FC<React.PropsWithChildren<unknown>> = ()
                         isExpanded={isRoleSelectOpen}
                         onClick={() => setRoleSelectOpen(!isRoleSelectOpen)}
                         isDisabled={roleSelectOptions.length === 0}
+                        isFullWidth
                       >
                         {modalSelectedRoleRef
                           ? roleMap.roleMap[modalSelectedRoleRef] ?? modalSelectedRoleRef
