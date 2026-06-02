fca5a8c1432e1c4575a4ef80f43c164b9fec2d02
Rodrigo Rosatti Galvão <rgalvao@redhat.com>
2026-06-02 15:01:12 +0200

    feat(KFLUXUI-231): [Release Details] add `YAML` tab (#207)
    * chore: add editor and YAML-related dependencies
    
    Added the following libraries:
    - `@patternfly/react-code-editor`: wrapper component around Monaco Editor for
    integration with PatternFly.
    - `yaml`: for parsing and stringifying YAML content.
    - `monaco-editor`: core Monaco Editor package
    - `monaco-editor-webpack-plugin`: enables custom Monaco configuration and
    language support via Webpack
    - `monaco-yaml`: provides YAML language support for Monaco Editor
    - `openapi-types`: type definitions for OpenAPI schema support.
    
    * chore(webpack): configure monaco-editor to load locally with custom workers
    
    Set up Monaco Editor to load locally instead of via CDN by configuring
    "@monaco-editor/react" with the "monaco-editor" package and
    "monaco-editor-webpack-plugin".
    This includes adding support for custom languages (e.g., YAML) with custom
    workers, and adjusting style-loader rules to handle Monaco’s CSS separately.
    
    * feat(shared/components/code-editor): add support for YAML file display
    
    Introduce new components based on PatternFly's CodeEditor to render
    code files. Currently, only ".yaml" files are supported via the
    YAMLCodeEditor component. Includes reusable hooks, types, and shortcuts
    to support editor functionality.
    
    * test(shared/components/code-editor): add tests for YAMLCodeEditor and CodeEditor
    
    Add a few tests to cover the usability of both 'YAMLCodeEditor' and
    'CodeEditor' components.
    
    * feat(Releases/ReleaseYamlTab): add tab to view release YAML
    
    Added a new "Release YAML" tab to display the YAML representation of the
    respective release.
    
    - uses the `yaml` library to stringify the release object
    - displays the YAML content in a "Monaco Editor" component with read-only mode.
    
    * feat(Releases): add "YAML" tab to release details page
    
    This patch has the necessary changes to display the "YAML" tab in the "Release
    Details" page.
    
    * feat(Releases/__tests__): add tests for ReleaseYamlTab component
    
    Add a few tests to cover some rendering scenarios of the 'ReleaseYamlTab'
    component.
    
    * fixup! chore(webpack): configure monaco-editor to load locally with custom workers
    
    * fixup! feat(shared/components/code-editor): add support for YAML file display
    
    * fixup! test(shared/components/code-editor): add tests for YAMLCodeEditor and CodeEditor
    
    * fixup! feat(Releases/ReleaseYamlTab): add tab to view release YAML
    
    * fixup! feat(Releases/__tests__): add tests for ReleaseYamlTab component
    
    * fixup! test(shared/components/code-editor): add tests for YAMLCodeEditor and CodeEditor
    
    * fixup! test(shared/components/code-editor): add tests for YAMLCodeEditor and CodeEditor

---
+++ src/shared/components/code-editor/YAMLCodeEditor.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type { Monaco } from '@monaco-editor/react';
+import { Language } from '@patternfly/react-code-editor';
+import { stringify as yamlStringify } from 'yaml';
+import { CodeEditor, type Props as CodeEditorProps } from './CodeEditor';
+import { useSwaggerDefinitions } from './hooks/useSwaggerDefinitions';
+import { registerYAMLinMonaco } from './monaco';
+
+type Props = Omit<CodeEditorProps, 'code' | 'onEditorDidMount'> & {
+  code: Record<string, unknown> | undefined;
+};
+
+export const YAMLCodeEditor: React.FC<Props> = ({ code, ...props }) => {
+  const { data: swaggerDefinitions } = useSwaggerDefinitions();
+
+  const [monacoInstance, setMonacoInstance] = React.useState<Monaco | null>(null);
+
+  const handleEditorDidMount = (monaco: Monaco) => {
+    if (!monaco) return;
+    setMonacoInstance(monaco);
+  };
+
+  React.useEffect(() => {
+    if (monacoInstance && swaggerDefinitions) {
+      registerYAMLinMonaco(monacoInstance, swaggerDefinitions);
+    }
+  }, [monacoInstance, swaggerDefinitions]);
+
+  return (
+    <CodeEditor
+      {...props}
+      language={Language.yaml}
+      code={yamlStringify(code ?? {})}
+      onEditorDidMount={(_, monaco) => handleEditorDidMount(monaco)}
+    />
+  );
+};
