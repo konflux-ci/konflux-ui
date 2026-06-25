77f8ccf1a799b2b393b1e2ea139f8cd7ce12608d
Abhi <abhinandan13jan@gmail.com>
2026-06-25 19:47:31 +0530

    feat(KFLUXUI-1451): update issues navItem with visual indicators

--- src/components/Issues/IssuesNavItemContent.tsx
+++ src/components/Issues/IssuesNavItemContent.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Icon } from '@patternfly/react-core';
-import { CheckCircleIcon } from '@patternfly/react-icons/dist/esm/icons/check-circle-icon';
 import { ExclamationCircleIcon } from '@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon';
 import { ExclamationTriangleIcon } from '@patternfly/react-icons/dist/esm/icons/exclamation-triangle-icon';
-import { InfoCircleIcon } from '@patternfly/react-icons/dist/esm/icons/info-circle-icon';
 import { IssueSeverity, IssueState } from '~/kite/issue-type';
 import { useIssues } from '~/kite/kite-hooks';
 
@@ -17,13 +15,7 @@ export const IssueNavItemContent: React.FC<{ namespace: string }> = ({ namespace
     if (isLoading || error) {
       return null;
     }
-    if (issues.length === 0) {
-      return (
-        <Icon status="success">
-          <CheckCircleIcon />
-        </Icon>
-      );
-    }
+
     const hasCriticalIssues = issues.find(
       (issue) => issue.severity === IssueSeverity.CRITICAL && issue.state !== IssueState.RESOLVED,
     );
@@ -44,17 +36,8 @@ export const IssueNavItemContent: React.FC<{ namespace: string }> = ({ namespace
         </Icon>
       );
     }
-    return (
-      <Icon status="info">
-        <InfoCircleIcon />
-      </Icon>
-    );
+    return null;
   }, [isLoading, error, issues]);
 
-  return (
-    <>
-      Issues
-      {statusIcon}
-    </>
-  );
+  return <>Issues {statusIcon}</>;
 };
