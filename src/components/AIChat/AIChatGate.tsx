e1beaff69af1c00f59eab95f4f2b56cb72694fe3
StanleyJochman <sjochman@redhat.com>
2026-09-08 11:28:34 +0200

    suggestions fix

--- src/components/AIChat/AIChatGate.tsx
+++ src/components/AIChat/AIChatGate.tsx
@@ -3,7 +3,9 @@ import { useIsLightspeedAvailable } from '~/components/AIChat/conditional-checks
 import { IfFeature } from '~/feature-flags/hooks';
 
 const AIChatDock = React.lazy(() =>
-  import('~/components/AIChat/AIChatDock').then((module) => ({ default: module.AIChatDock })),
+  import(
+    '~/components/AIChat/AIChatDock' /* webpackChunkName: "ai-chat-dock" */
+  ).then((module) => ({ default: module.AIChatDock })),
 );
 
 const AIChatDockLoader: React.FC = () => {
