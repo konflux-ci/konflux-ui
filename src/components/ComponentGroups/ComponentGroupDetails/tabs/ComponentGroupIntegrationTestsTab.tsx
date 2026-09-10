739411d42ab141812e03918146f32fccbda93c14
Rodrigo R. Galvão <rgalvao@redhat.com>
2026-09-10 12:30:32 +0200

    fixup! feat(ComponentGroupIntegrationTestsTab): add component

--- src/components/ComponentGroups/ComponentGroupDetails/tabs/ComponentGroupIntegrationTestsTab.tsx
+++ src/components/ComponentGroups/ComponentGroupDetails/tabs/ComponentGroupIntegrationTestsTab.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import { IfFeature } from '~/feature-flags/hooks';
 import IntegrationTestsListViewV2 from '../../../IntegrationTests/IntegrationTestsListView/IntegrationTestsListViewV2';
 
 export const ComponentGroupIntegrationTestsTab: React.FC = () => {
-  return <IntegrationTestsListViewV2 />;
+  return (
+    <IfFeature flag="component-model">
+      <IntegrationTestsListViewV2 />
+    </IfFeature>
+  );
 };
 
 export default ComponentGroupIntegrationTestsTab;
