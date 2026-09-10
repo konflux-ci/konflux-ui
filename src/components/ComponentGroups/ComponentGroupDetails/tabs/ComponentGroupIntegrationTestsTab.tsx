581864f0f89324f302a870d5b8dc0278524ac92d
Rodrigo R. Galvão <rgalvao@redhat.com>
2026-09-10 11:24:18 +0200

    feat(ComponentGroupIntegrationTestsTab): add component
    The ComponentGroupIntegrationTestsTab component is a simple wrapper around the
    IntegrationTestsListViewV2 component.

---
+++ src/components/ComponentGroups/ComponentGroupDetails/tabs/ComponentGroupIntegrationTestsTab.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import IntegrationTestsListViewV2 from '../../../IntegrationTests/IntegrationTestsListView/IntegrationTestsListViewV2';
+
+export const ComponentGroupIntegrationTestsTab: React.FC = () => {
+  return <IntegrationTestsListViewV2 />;
+};
+
+export default ComponentGroupIntegrationTestsTab;
